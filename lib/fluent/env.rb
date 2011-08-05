07b19739047e82b7e0dcd9d40eed1d9512ca12bb
FURUHASHI Sadayuki <frsyuki@users.sourceforge.jp>
2011-08-06 02:43:02 +0900

    use /var/run/fluent/fluent.sock for default path of in_unix

--- lib/fluent/env.rb
+++ lib/fluent/env.rb
@@ -1,6 +1,6 @@
 module Fluent
 DEFAULT_CONFIG_PATH = ENV['FLUENT_CONF'] || '/etc/fluent/fluent.conf'
 DEFAULT_PLUGIN_DIR = ENV['FLUENT_PLUGIN'] || '/etc/fluent/plugin'
-DEFAULT_SOCKET_PATH = ENV['FLUENT_SOCKET'] || '/var/run/fluent.sock'
+DEFAULT_SOCKET_PATH = ENV['FLUENT_SOCKET'] || '/var/run/fluent/fluent.sock'
 DEFAULT_LISTEN_PORT = 24224
 end
