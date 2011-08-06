99e619ca52fd01dc994b36d5b0d4b81b2bb06b04
FURUHASHI Sadayuki <frsyuki@users.sourceforge.jp>
2011-08-06 19:28:07 +0900

    out_tdlog: check table name before emitting

--- lib/fluent/plugin/out_tdlog.rb
+++ lib/fluent/plugin/out_tdlog.rb
@@ -30,12 +30,14 @@ class TreasureDataLogOutput < BufferedOutput
     require 'tempfile'
     require 'zlib'
     require 'net/http'
+    require 'json'
     require 'cgi' # CGI.escape
     require 'time' # Time#rfc2822
     super
     @tmpdir = '/tmp/fluent/tdlog'
     @apikey = nil
     @key = nil
+    @table_list = []
   end
 
   def configure(conf)
@@ -62,12 +64,15 @@ class TreasureDataLogOutput < BufferedOutput
     elsif (database && !table) || (!database && table)
       raise ConfigError, "'database' and 'table' parameter are required on tdlog output"
     end
+
+    @table_list = get_table_list
+
+    if @key && !@table_list.include?(@key)
+      raise ConfigError, "Table #{@key.inspect} does not exist on Treasure Data. Use 'td create-log-table #{database} #{table}' to create it."
+    end
   end
 
   def emit(tag, es, chain)
-    # TODO check table name is in the list
-    # TODO if not exists, get table list from API server
-    # TODO if not exists, raise error
     if @key
       key = @key
     else
@@ -79,11 +84,24 @@ class TreasureDataLogOutput < BufferedOutput
       key = "#{database}.#{table}"
     end
 
+    # check the table exists
+    unless @table_list.include?(key)
+      begin
+        @table_list = get_table_list
+      rescue
+        $log.warn "failed to update table list on Treasure Data", :error=>$!.to_s
+        $log.debug_backtrace $!
+      end
+      unless @table_list.include?(key)
+        database, table = key.split('.',2)
+        raise "Table #{key.inspect} does not exist on Treasure Data. Use 'td create-log-table #{database} #{table}' to create it."
+      end
+    end
+
     super(tag, es, chain, key)
   end
 
   def validate_name(name)
-    # TODO
     true
   end
 
@@ -121,6 +139,66 @@ class TreasureDataLogOutput < BufferedOutput
   end
 
   def upload(database, table, io, size)
+    http, header = new_http
+    header['Content-Length'] = size.to_s
+    header['Content-Type'] = 'application/octet-stream'
+
+    url = "/v3/table/import/#{e database}/#{e table}/msgpack.gz"
+
+    req = Net::HTTP::Put.new(url, header)
+    if req.respond_to?(:body_stream=)
+      req.body_stream = io
+    else  # Ruby 1.8
+      req.body = io.read
+    end
+
+    $log.trace { "uploading logs to Treasure Data database=#{database} table=#{table} (#{size}bytes)" }
+
+    response = http.request(req)
+
+    if response.code[0] != ?2
+      raise "Treasure Data upload failed: #{response.body}"
+    end
+  end
+
+  def get_table_list
+    $log.info "updating table list from Treasure Data"
+    list = []
+    api_list_database.each {|db|
+      api_list_table(db).each {|t|
+        list << "#{db}.#{t}"
+      }
+    }
+    list
+  end
+
+  def api_list_database
+    body = get("/v3/database/list")
+    js = JSON.load(body)
+    return js["databases"].map {|m| m['name'] }
+  end
+
+  def api_list_table(db)
+    body = get("/v3/table/list/#{e db}")
+    js = JSON.load(body)
+    return js["tables"].map {|m| m['name'] }
+  end
+
+  def get(path)
+    http, header = new_http
+
+    request = Net::HTTP::Get.new(path, header)
+
+    response = http.request(request)
+
+    if response.code[0] != ?2
+      raise "Treasure Data API failed: #{response.body}"
+    end
+
+    return response.body
+  end
+
+  def new_http
     http = Net::HTTP.new(HOST, PORT)
     if USE_SSL
       http.use_ssl = true
@@ -135,25 +213,8 @@ class TreasureDataLogOutput < BufferedOutput
     header = {}
     header['Authorization'] = "TD1 #{@apikey}"
     header['Date'] = Time.now.rfc2822
-    header['Content-Length'] = size.to_s
-    header['Content-Type'] = 'application/octet-stream'
 
-    url = "/v3/table/import/#{e database}/#{e table}/msgpack.gz"
-
-    req = Net::HTTP::Put.new(url, header)
-    if req.respond_to?(:body_stream=)
-      req.body_stream = io
-    else  # Ruby 1.8
-      req.body = io.read
-    end
-
-    $log.trace { "uploading logs to TreasureData database=#{database} table=#{table} (#{size}bytes)" }
-
-    response = http.request(req)
-
-    if response.code[0] != ?2
-      raise "TreasureData upload failed: #{response.body}"
-    end
+    return http, header
   end
 
   def e(s)
