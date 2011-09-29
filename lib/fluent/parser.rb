9ea9dd641963b4290ec1b9fbf3e18b1cf4b42668
Eiichiro Iwata <eiichiro.iwata+github@gmail.com>
2011-09-30 05:40:54 +0900

    fixed TextParser to register template correctly

--- lib/fluent/parser.rb
+++ lib/fluent/parser.rb
@@ -24,7 +24,7 @@ class TextParser
     'syslog' => [/^(?<time>[^ ]* [^ ]* [^ ]*) (?<host>[^ ]*) (?<ident>[a-zA-Z0-9_\/\.\-]*)(?:\[(?<pid>[0-9]+)\])?[^\:]*\: *(?<message>.*)$/, "%b %d %H:%M:%S"],
   }
 
-  def self.register_template(name, regexp)
+  def self.register_template(name, regexp, time_format)
     TEMPLATES[name] = [regexp, time_format]
   end
 
