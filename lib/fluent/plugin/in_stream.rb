325cfd6e6766f1553ca7942d4987267276d70080
FURUHASHI Sadayuki <frsyuki@users.sourceforge.jp>
2012-04-17 11:22:19 -0700

    in_unix is not obsoleted

--- lib/fluent/plugin/in_stream.rb
+++ lib/fluent/plugin/in_stream.rb
@@ -183,7 +183,7 @@ class UnixInput < StreamInput
 
   def configure(conf)
     super
-    $log.warn "'unix' input is obsoleted and will be removed. Use 'forward' instead."
+    #$log.warn "'unix' input is obsoleted and will be removed. Use 'forward' instead."
   end
 
   def listen
