9f2e1e2cb0e24b140643dedfd3b18aac34f6591a
Masahiro Nakagawa <repeatedly@gmail.com>
2012-12-03 19:45:39 +0900

    Forgot to add 'IPSocket.getaddress' to resolve host

--- lib/fluent/plugin/socket_util.rb
+++ lib/fluent/plugin/socket_util.rb
@@ -3,7 +3,7 @@ module Fluent
     def create_udp_socket(host)
       require 'ipaddr'
 
-      if IPAddr.new(host).ipv4?
+      if IPAddr.new(IPSocket.getaddress(host)).ipv4?
         UDPSocket.new
       else
         UDPSocket.new(Socket::AF_INET6)
