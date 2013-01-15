39e31928a65d5a553fc990ea3bdf973f89826123
FURUHASHI Sadayuki <frsyuki@users.sourceforge.jp>
2013-01-15 01:03:09 -0800

    added IdMixin

--- lib/fluent/mixin.rb
+++ lib/fluent/mixin.rb
@@ -217,4 +217,24 @@ module SetTagKeyMixin
   end
 end
 
+
+module IdMixin
+  def configure(conf)
+    @id = conf['id']
+    super
+  end
+
+  def require_id
+    unless @id
+      raise ConfigError, "'id' parameter is required"
+    end
+    @id
+  end
+
+  def plugin_id
+    @id ? @id : "object:#{object_id.to_s(16)}"
+  end
+end
+
+
 end
