db9b6eb8b4411ed099a00354a3f2b6afcdc51c0e
Kentaro Kuribayashi <kentarok@gmail.com>
2013-02-07 00:10:09 +0900

    Added LabeledTSVParser and a test for it.

---
+++ test/parser.rb
@@ -0,0 +1,50 @@
+require 'helper'
+require 'fluent/test'
+require 'fluent/parser'
+
+module ParserTest
+  class LabeledTSVParserParserTest < Test::Unit::TestCase
+    include Fluent
+
+    def test_config_params
+      parser = Fluent::TextParser::LabeledTSVParser.new
+
+      assert_equal "\t", parser.delimiter
+      assert_equal  ":", parser.label_delimiter
+
+      parser.configure(
+        'delimiter'       => ',',
+        'label_delimiter' => '=',
+      )
+
+      assert_equal ",", parser.delimiter
+      assert_equal "=", parser.label_delimiter
+    end
+
+    def test_call
+      parser = Fluent::TextParser::LabeledTSVParser.new
+      time, record = parser.call('time:[28/Feb/2013:12:00:00 +0900]	host:192.168.0.1	req:GET /list HTTP/1.1')
+
+      assert_equal({
+        'time' => '[28/Feb/2013:12:00:00 +0900]',
+        'host' => '192.168.0.1',
+        'req'  => 'GET /list HTTP/1.1',
+      }, record)
+    end
+
+    def test_call_with_customized_delimiter
+      parser = Fluent::TextParser::LabeledTSVParser.new
+      parser.configure(
+        'delimiter'       => ',',
+        'label_delimiter' => '=',
+      )
+      time, record = parser.call('time=[28/Feb/2013:12:00:00 +0900],host=192.168.0.1,req=GET /list HTTP/1.1')
+
+      assert_equal({
+        'time' => '[28/Feb/2013:12:00:00 +0900]',
+        'host' => '192.168.0.1',
+        'req'  => 'GET /list HTTP/1.1',
+      }, record)
+    end
+  end
+end
