3dc054080722ddd1eacedfda37538cf80cbb2a21
Masahiro Nakagawa <repeatedly@gmail.com>
2013-02-17 05:52:40 +0900

    Add in_gc_stat and in_status tests. fixes #63

---
+++ test/plugin/in_gc_stat.rb
@@ -0,0 +1,36 @@
+require 'fluent/test'
+
+class ObjectSpaceInputTest < Test::Unit::TestCase
+  def setup
+    Fluent::Test.setup
+  end
+
+  CONFIG = %[
+    emit_interval 1
+    tag t1
+  ]
+
+  def create_driver(conf=CONFIG)
+    Fluent::Test::InputTestDriver.new(Fluent::GCStatInput).configure(conf)
+  end
+
+  def test_configure
+    d = create_driver
+    assert_equal(1, d.instance.emit_interval)
+    assert_equal("t1", d.instance.tag)
+  end
+
+  def test_emit
+    stat = GC.stat
+    stub(GC).stat { stat }
+
+    d = create_driver
+    d.run do
+      sleep 2
+    end
+
+    emits = d.emits
+    assert(emits.length > 0)
+    assert_equal(stat, emits[0][2])
+  end
+end
