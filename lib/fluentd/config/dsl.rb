5a9bafa7f2a514bcb25a4d0806a27aca56396a9a
TAGOMORI Satoshi <tagomoris@gmail.com>
2013-08-12 17:40:32 +0900

    fix methods as private not to be accessed from dsl code

--- lib/fluentd/config/dsl.rb
+++ lib/fluentd/config/dsl.rb
@@ -9,7 +9,7 @@ module Fluentd
         end
 
         def self.parse(source, source_path="config.rb")
-          DSLElement.new('ROOT', nil).instance_eval(source, source_path).__config_element
+          DSLElement.new('ROOT', nil).instance_eval(source, source_path).__to_config_element
         end
       end
 
@@ -23,7 +23,7 @@ module Fluentd
           @elements = []
         end
 
-        def __config_element
+        def __to_config_element
           Fluentd::Config::Element.new(@name, @arg, @attrs, @elements)
         end
 
@@ -33,31 +33,33 @@ module Fluentd
           if block
             element = DSLElement.new(name.to_s, value)
             element.instance_exec(&block)
-            @elements.push(element.__config_element)
+            @elements.push(element.__to_config_element)
           else
             @attrs[name.to_s] = value.is_a?(Symbol) ? value.to_s : value
           end
           self
         end
 
-        def __element(name, arg, block)
+        def worker(&block); element('worker', nil, block); end
+        def source(&block); element('source', nil, block); end
+        def filter(*args, &block); need_arg('filter', args); element('filter', args.first, block); end
+        def match(*args, &block);  need_arg('match',  args); element('match',  args.first, block); end
+        def label(*args, &block);  need_arg('label',  args); element('label',  args.first, block); end
+
+        private
+
+        def element(name, arg, block)
           raise ArgumentError, "#{name} block must be specified" if block.nil?
           element = DSLElement.new(name.to_s, arg)
           element.instance_exec(&block)
-          @elements.push(element.__config_element)
+          @elements.push(element.__to_config_element)
           self
         end
 
-        def __need_arg(name, args)
+        def need_arg(name, args)
           raise ArgumentError, "#{name} block requires arguments for match pattern" if args.nil? || args.size != 1
           true
         end
-
-        def worker(&block); __element('worker', nil, block); end
-        def source(&block); __element('source', nil, block); end
-        def filter(*args, &block); __need_arg('filter', args); __element('filter', args.first, block); end
-        def match(*args, &block);  __need_arg('match',  args); __element('match',  args.first, block); end
-        def label(*args, &block);  __need_arg('label',  args); __element('label',  args.first, block); end
       end
     end
   end
