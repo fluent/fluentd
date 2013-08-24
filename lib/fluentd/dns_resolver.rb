916ccf688e6969517c9c5104753c94e289a551b4
TAGOMORI Satoshi <tagomoris@gmail.com>
2013-08-24 14:48:15 +0900

    add IPv4/IPv6 common resolver

---
+++ lib/fluentd/dns_resolver.rb
@@ -0,0 +1,40 @@
+require 'socket'
+require 'ipaddr'
+require 'resolv'
+
+module Fluentd
+  class ResolveError < StandardError
+  end
+
+  class DNSResolver
+    def initialize(proto=:ipv4)
+      @proto = proto
+      @resolver = Resolv::DNS.new
+    end
+
+    def ipv4? ; @proto == :ipv4 ; end
+    def ipv6? ; @proto == :ipv6 ; end
+
+    def resolve(name)
+      if name.downcase == 'localhost'
+        return ipv4? ? '127.0.0.1' : '::1'
+      end
+
+      begin
+        ipaddr = IPSocket.getaddress(name)
+        return ipaddr if ipv4? && IPAddr.new(ipaddr).ipv4? || ipv6? && IPAddr.new(ipaddr).ipv6?
+      rescue SocketError => e
+        raise unless e.message.start_with?('getaddrinfo: nodename nor servname provided, or not known')
+      end
+
+      t = ipv6? ? Resolv::DNS::Resource::IN::AAAA : Resolv::DNS::Resource::IN::A
+      begin
+        return Resolv::DNS.new.getresource(name, t).address.to_s
+      rescue Resolv::ResolvError => e
+        raise unless e.message.start_with?('DNS result has no information for')
+      end
+
+      raise ResolveError, "Failed to resolve host name #{name}"
+    end
+  end
+end
---
+++ spec/dns_resolver_spec.rb
@@ -0,0 +1,59 @@
+require 'fluentd/dns_resolver'
+require 'ipaddr'
+
+describe Fluentd::DNSResolver do
+  context 'for IPv4 addresses' do
+    describe '#resolve' do
+      r = Fluentd::DNSResolver.new(:ipv4)
+
+      it 'returns 127.0.0.1 for "localhost"' do
+        expect(r.resolve('localhost')).to eql('127.0.0.1')
+      end
+
+      it 'returns IPv4 address for example.com' do
+        expect(IPAddr.new(r.resolve('example.com')).ipv4?).to be_true
+      end
+
+      it 'returns IPv4 address for hostname of this host' do
+        hostname = `hostname`.chop
+        expect(IPAddr.new(r.resolve(hostname)).ipv4?).to be_true
+      end
+
+      it 'raises ResolveError for unknown host' do
+        expect { r.resolve('non.existing.hostname.example.com') }.to raise_error(Fluentd::ResolveError)
+      end
+    end
+  end
+
+  context 'for IPv6 addresses' do
+    describe '#resolve' do
+      r = Fluentd::DNSResolver.new(:ipv6)
+
+      it 'returns ::1 for "localhost"' do
+        expect(r.resolve('localhost')).to eql('::1')
+      end
+
+      it 'returns IPv6 address for example.com' do
+        expect(IPAddr.new(r.resolve('example.com')).ipv6?).to be_true
+      end
+
+      it 'returns IPv6 address for hostname of this host' do
+        hostname = `hostname`.chop
+        addr = begin
+                 r.resolve(hostname)
+               rescue Fluentd::ResolveError => e
+                 nil # ignore
+               end
+        if addr
+          expect(IPAddr.new(addr).ipv6?).to be_true
+        else
+          expect(addr).to be_nil # IPv6 not ready environment
+        end
+      end
+
+      it 'raises ResolveError for unknown host' do
+        expect { r.resolve('non.existing.hostname.example.com') }.to raise_error(Fluentd::ResolveError)
+      end
+    end
+  end
+end
