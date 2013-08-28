ecae0a21837c60ab62bdb43cd4d4f142d8b3ad63
Mitsunori Komatsu <komamitsu@gmail.com>
2013-08-29 01:19:12 +0900

    Plugin::TailInput uses Util::TextParser

--- lib/fluentd/plugin/util/text_parser.rb
+++ lib/fluentd/plugin/util/text_parser.rb
@@ -15,6 +15,8 @@
 #    See the License for the specific language governing permissions and
 #    limitations under the License.
 #
+require 'time'
+
 module Fluentd
   module Plugin
     module Util
