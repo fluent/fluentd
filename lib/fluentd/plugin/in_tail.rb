7fddbb83e15833231320883477813c694b2317ae
FURUHASHI Sadayuki <frsyuki@users.sourceforge.jp>
2013-11-13 00:44:24 -0800

    bulk updated copyright comments

--- lib/fluentd/plugin/in_tail.rb
+++ lib/fluentd/plugin/in_tail.rb
@@ -1,8 +1,6 @@
 #
 # Fluent
 #
-# Copyright (C) 2011 FURUHASHI Sadayuki
-#
 #    Licensed under the Apache License, Version 2.0 (the "License");
 #    you may not use this file except in compliance with the License.
 #    You may obtain a copy of the License at
--- lib/fluentd/plugin/routing_output.rb
+++ lib/fluentd/plugin/routing_output.rb
@@ -1,8 +1,6 @@
 #
 # Fluentd
 #
-# Copyright (C) 2011-2013 FURUHASHI Sadayuki
-#
 #    Licensed under the Apache License, Version 2.0 (the "License");
 #    you may not use this file except in compliance with the License.
 #    You may obtain a copy of the License at
