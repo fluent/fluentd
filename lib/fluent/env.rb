dd74b91c3c6828817851a52d10ce8606d65a3be9
okahashi117 <naruki_okahashi@jbat.co.jp>
2014-01-29 18:21:12 +0900

    Add windows service skelton file for fluentd. Not work yet as windows service, Need set info to registry manualy. Please wait a register code, comming soon.

--- lib/fluent/env.rb
+++ lib/fluent/env.rb
@@ -5,4 +5,5 @@ DEFAULT_SOCKET_PATH = ENV['FLUENT_SOCKET'] || '/var/run/fluent/fluent.sock'
 DEFAULT_LISTEN_PORT = 24224
 DEFAULT_FILE_PERMISSION = 0644
 RUBY_INSTALL_DIR = 'C:/Ruby193'
+FLUENTD_OPTION_FOR_WINSVC = '-c C:/Ruby193/fluent/fluent.conf -o C:/Ruby193/fluent/fluentwinsvc.log'
 end
---
+++ lib/fluent/winsvc.rb
@@ -0,0 +1,35 @@
+
+INTEVENTOBJ_NAME="fluentdwinsvc"
+
+module Fluent
+
+begin
+require "win32/daemon"
+require "fluent/env"
+require "fluent/win32api_syncobj"
+
+include Win32
+ 
+class FluentdService < Daemon
+  def service_main
+    Process.spawn (Fluent::RUBY_INSTALL_DIR+"/bin/ruby.exe '"+Fluent::RUBY_INSTALL_DIR+"/bin/fluentd' "+Fluent::FLUENTD_OPTION_FOR_WINSVC+" -s "+INTEVENTOBJ_NAME)
+    while running?
+      sleep 10
+    end
+  end
+ 
+  def service_stop
+    sigint = Win32SyncObj.createevent(1,0,INTEVENTOBJ_NAME)
+    sigint.signal_on
+    sigint.close
+    exit!
+  end
+end
+
+FluentdService.mainloop
+
+rescue Execption => err
+  raise
+end
+
+end
