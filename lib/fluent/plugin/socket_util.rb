7dac46123da6bd4761987ac64d7111c7ee8d9b4b
sonots <sonots@gmail.com>
2014-01-31 21:28:14 +0900

    create_tcp_socket timeout

--- lib/fluent/plugin/socket_util.rb
+++ lib/fluent/plugin/socket_util.rb
@@ -10,5 +10,57 @@ module Fluent
       end
     end
     module_function :create_udp_socket
+
+    # Create TCP Socket
+    #
+    # Example:
+    #
+    #     begin
+    #       SocketUtil.create_tcp_socket(host, port, connect_timeout: 0.5)
+    #     rescue Timeout::Error
+    #       log.debug "Timeout!"
+    #     end
+    #
+    # @param [String] host
+    # @param [Integer] port
+    # @param [Hash] opts
+    # @return [Socket]
+    #
+    # cf. https://bugs.ruby-lang.org/issues/5101
+    def create_tcp_socket(host, port, opts={})
+      connect_timeout = opts[:connect_timeout] || 5.0
+      addr = Socket.pack_sockaddr_in(port, host)
+      s = Socket.new(:AF_INET, :SOCK_STREAM, 0)
+      begin
+        s.connect_nonblock(addr)
+      rescue Errno::EINPROGRESS
+        IO.select(nil, [s], nil, connect_timeout) or raise Timeout::Error
+      end
+      s
+    end
+    module_function :create_tcp_socket
+
+    # Open TCP Socket
+    #
+    # Example:
+    #
+    #     begin
+    #       SocketUtil.open_tcp_socket(host, port, connect_timeout: 0.5) {|sock| }
+    #     rescue Timeout::Error
+    #       log.debug "Timeout!"
+    #     end
+    #
+    # @param [String] host
+    # @param [Integer] port
+    # @param [Hash] opts
+    def open_tcp_socket(host, port, opts={}, &block)
+      s = create_tcp_socket(host, port, opts)
+      begin
+        yield s
+      ensure
+        s.close
+      end
+    end
+    module_function :open_tcp_socket
   end
 end
