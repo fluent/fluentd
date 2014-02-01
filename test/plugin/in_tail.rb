99e0936e30dc89798e02644c9db382dce26607e1
Jun Hiroe <Jun.Hiroe@gmail.com>
2014-02-01 21:23:29 +0900

    Add rotate tests in in_tail plugin

--- test/plugin/in_tail.rb
+++ test/plugin/in_tail.rb
@@ -18,6 +18,13 @@ class TailInputTest < Test::Unit::TestCase
     format /(?<message>.*)/
   ]
 
+  CONFIG_WITHOUT_POS_FILE = %[
+    path #{TMP_DIR}/tail.txt
+    tag t1
+    rotate_wait 2s
+    format /(?<message>.*)/
+  ]
+
   def create_driver(conf=CONFIG)
     Fluent::Test::InputTestDriver.new(Fluent::TailInput).configure(conf)
   end
@@ -103,5 +110,101 @@ class TailInputTest < Test::Unit::TestCase
     assert_equal({"message"=>"	"}, emits[3][2])
     assert_equal({"message"=>"	tab"}, emits[4][2])
     assert_equal({"message"=>"tab	"}, emits[5][2])
+  end
+
+  def test_rotate_file
+    sub_test_rotate_file(CONFIG)
+  end
+
+  def test_rotate_file_without_pos_file
+    sub_test_rotate_file(CONFIG_WITHOUT_POS_FILE)
+  end
+
+  def test_rotate_file_when_fluent_doesnot_start
+    sub_test_rotate_file_when_fluent_doesnot_start(CONFIG)
+  end
+
+  def sub_test_rotate_file(config)
+    File.open("#{TMP_DIR}/tail.txt", "w") {|f|
+      f.puts "test1"
+      f.puts "test2"
+    }
+
+    d = create_driver(config)
+
+    d.run do
+      sleep 1
+
+      File.open("#{TMP_DIR}/tail.txt", "a") {|f|
+        f.puts "test3"
+        f.puts "test4"
+      }
+      sleep 1
+
+      FileUtils.mv("#{TMP_DIR}/tail.txt", "#{TMP_DIR}/tail2.txt")
+      sleep 1
+
+      File.open("#{TMP_DIR}/tail.txt", "w") {|f| }
+      sleep 1
+
+      File.open("#{TMP_DIR}/tail.txt", "a") {|f|
+        f.puts "test5"
+        f.puts "test6"
+      }
+      sleep 1
+    end
+
+    d.run do
+      sleep 1
+    end
+
+    emits = d.emits
+    assert_equal(true, emits.length > 0)
+    assert_equal({"message"=>"test3"}, emits[0][2])
+    assert_equal({"message"=>"test4"}, emits[1][2])
+    assert_equal({"message"=>"test5"}, emits[2][2])
+    assert_equal({"message"=>"test6"}, emits[3][2])
+  end
+
+  # A log file rotates when fluentd does not start.
+  def sub_test_rotate_file_when_fluent_doesnot_start(config)
+    File.open("#{TMP_DIR}/tail.txt", "w") {|f|
+      f.puts "test1"
+      f.puts "test2"
+    }
+
+    d = create_driver(config)
+
+    d.run do
+      sleep 1
+
+      File.open("#{TMP_DIR}/tail.txt", "a") {|f|
+        f.puts "test3"
+        f.puts "test4"
+      }
+      sleep 1
+
+      FileUtils.mv("#{TMP_DIR}/tail.txt", "#{TMP_DIR}/tail2.txt")
+    end
+
+    File.open("#{TMP_DIR}/tail.txt", "w") {|f| }
+    sleep 1
+
+    File.open("#{TMP_DIR}/tail.txt", "a") {|f|
+      f.puts "test5"
+      f.puts "test6"
+    }
+    sleep 1
+
+    d.run do
+      sleep 1
+    end
+
+    emits = d.emits
+    assert_equal(true, emits.length > 0)
+    assert_equal({"message"=>"test3"}, emits[0][2])
+    assert_equal({"message"=>"test4"}, emits[1][2])
+    assert_equal({"message"=>"test5"}, emits[2][2])
+    assert_equal({"message"=>"test6"}, emits[3][2])
   end
 end
