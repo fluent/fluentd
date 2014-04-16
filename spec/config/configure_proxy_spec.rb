bd608bfb0d78e8b2133ed3521088f0b6475db771
TAGOMORI Satoshi <tagomoris@gmail.com>
2014-04-16 23:06:22 +0900

    add specs for default behavior of ConfigureProxy, and its merging

---
+++ spec/config/configure_proxy_spec.rb
@@ -0,0 +1,72 @@
+require 'fluent/config/configure_proxy'
+
+describe Fluent::Config::ConfigureProxy do
+  context 'to generate a instance' do
+    describe '#initialize' do
+      it 'has default values' do
+        proxy = Fluent::Config::ConfigureProxy.new('section')
+        expect(proxy.name).to eql(:section)
+
+        proxy = Fluent::Config::ConfigureProxy.new(:section)
+        expect(proxy.name).to eql(:section)
+        expect(proxy.param_name).to eql(:section)
+        expect(proxy.required).to be_nil
+        expect(proxy.required?).to be_false
+        expect(proxy.multi).to be_nil
+        expect(proxy.multi?).to be_true
+      end
+
+      it 'can specify param_name/required/multi with optional arguments' do
+        proxy = Fluent::Config::ConfigureProxy.new(:section, param_name: 'sections', required: false, multi: true)
+        expect(proxy.name).to eql(:section)
+        expect(proxy.param_name).to eql(:sections)
+        expect(proxy.required).to be_false
+        expect(proxy.required?).to be_false
+        expect(proxy.multi).to be_true
+        expect(proxy.multi?).to be_true
+
+        proxy = Fluent::Config::ConfigureProxy.new(:section, param_name: :sections, required: true, multi: false)
+        expect(proxy.name).to eql(:section)
+        expect(proxy.param_name).to eql(:sections)
+        expect(proxy.required).to be_true
+        expect(proxy.required?).to be_true
+        expect(proxy.multi).to be_false
+        expect(proxy.multi?).to be_false
+      end
+    end
+
+    describe '#merge' do
+      it 'generate a new instance which values are overwritten by the argument object' do
+        proxy = p1 = Fluent::Config::ConfigureProxy.new(:section)
+        expect(proxy.name).to eql(:section)
+        expect(proxy.param_name).to eql(:section)
+        expect(proxy.required).to be_nil
+        expect(proxy.required?).to be_false
+        expect(proxy.multi).to be_nil
+        expect(proxy.multi?).to be_true
+
+        p2 = Fluent::Config::ConfigureProxy.new(:section, param_name: :sections, required: true, multi: false)
+        proxy = p1.merge(p2)
+        expect(proxy.name).to eql(:section)
+        expect(proxy.param_name).to eql(:sections)
+        expect(proxy.required).to be_true
+        expect(proxy.required?).to be_true
+        expect(proxy.multi).to be_false
+        expect(proxy.multi?).to be_false
+      end
+
+      it 'does not overwrite with argument object without any specifications of required/multi' do
+        p1 = Fluent::Config::ConfigureProxy.new(:section1)
+        p2 = Fluent::Config::ConfigureProxy.new(:section2, param_name: :sections, required: true, multi: false)
+        p3 = Fluent::Config::ConfigureProxy.new(:section3)
+        proxy = p1.merge(p2).merge(p3)
+        expect(proxy.name).to eql(:section3)
+        expect(proxy.param_name).to eql(:section3)
+        expect(proxy.required).to be_true
+        expect(proxy.required?).to be_true
+        expect(proxy.multi).to be_false
+        expect(proxy.multi?).to be_false
+      end
+    end
+  end
+end
