356e6c13173e8426c7fef3d3eda6d2e00a5a2f50
TAGOMORI Satoshi <tagomoris@gmail.com>
2014-04-16 18:39:42 +0900

    Add ConfigureProxy and to use it to build configuration tree  * receivers are required to call config_section recursively, instead of plugin instance  * After configuration tree build process, root section node is combined to plugin instance    * by setting root node parameters into instance variables
    All existing tests have passed at this commit.

---
+++ lib/fluent/config/section.rb
@@ -0,0 +1,74 @@
+module Fluent
+  require 'fluent/config/error'
+
+  module Config
+    module SectionGenerator
+      def self.generate(proxy, conf, logger, stack=[])
+        return nil if conf.nil?
+
+        section_params = {}
+
+        proxy.defaults.each_pair do |name, defval|
+          varname = name.to_sym
+          section_params[varname] = defval
+        end
+
+        proxy.params.each_pair do |name, defval|
+          varname = name.to_sym
+          block,opts = defval
+          if val = conf[name.to_s]
+            section_params[varname] = self.instance_exec(val, opts, name, &block)
+          end
+          unless section_params.has_key?(varname)
+            logger.error "config error in:\n#{conf}"
+            raise ConfigError, "'#{name}' parameter is required"
+          end
+        end
+
+        proxy.sections.each do |name, subproxy|
+          varname = subproxy.param_name.to_sym
+          elements = (conf.respond_to?(:elements) ? conf.elements : []).select{|e| e.name == subproxy.name.to_s }
+
+          if subproxy.required? && elements.size < 1
+            logger.error "config error in:\n#{conf}"
+            raise ConfigError, "'<#{proxy.name}>' section is required"
+          end
+          if subproxy.multi?
+            section_params[varname] = elements.map{ |e| generate(proxy.h, e, logger) }
+          else
+            if elements.size > 1
+              logger.error "config error in:\n#{conf}"
+              raise ConfigError, "'<#{subproxy.name}>' section cannot be written twice or more"
+            end
+            section_params[varname] = generate(proxy, elements.first, logger)
+          end
+        end
+
+        Section.new(section_params)
+      end
+    end
+
+    class Section < BasicObject
+      # to get parameter list, see instance methods
+      def initialize(params)
+        @params = params
+      end
+
+      def [](key)
+        @params[key.to_sym]
+      end
+
+      def respond_to_missing?(symbol, include_private)
+        @params.has_key?(symbol)
+      end
+
+      def method_missing(name, *args)
+        if @params.has_key?(name)
+          @params[name]
+        else
+          super
+        end
+      end
+    end
+  end
+end
--- lib/fluent/configurable.rb
+++ lib/fluent/configurable.rb
@@ -1,4 +1,5 @@
 module Fluent
+  require 'fluent/config/section'
   require 'fluent/config/error'
   require 'fluent/registry'
 
@@ -10,26 +11,38 @@ module Fluent
     end
 
     def initialize
-      self.class.config_defaults.each_pair { |name, defval|
-        varname = :"@#{name}"
-        instance_variable_set(varname, defval)
-      }
+      # to simulate implicit 'attr_accessor' by config_param / config_section and its value by config_set_default
+      proxy = self.class.merged_configure_proxy
+      proxy.params.keys.each do |name|
+        if proxy.defaults.has_key?(name)
+          instance_variable_set("@#{name}".to_sym, proxy.defaults[name])
+        end
+      end
+      proxy.sections.keys.each do |name|
+        subproxy = proxy.sections[name]
+        if subproxy.multi?
+          instance_variable_set("@#{subproxy.param_name}".to_sym, [])
+        else
+          instance_variable_set("@#{subproxy.param_name}".to_sym, nil)
+        end
+      end
     end
 
     def configure(conf)
       @config = conf
 
-      self.class.config_params.each_pair { |name, (block,opts)|
-        varname = :"@#{name}"
-        if val = conf[name.to_s]
-          val = self.instance_exec(val, opts, name, &block)
-          instance_variable_set(varname, val)
+      logger = self.respond_to?(:log) ? log : $log
+      proxy = self.class.merged_configure_proxy
+
+      root = Fluent::Config::SectionGenerator.generate(proxy, conf, logger)
+      @config_root_section = root
+
+      root.instance_eval{ @params.keys }.each do |param_name|
+        varname = "@#{param_name}".to_sym
+        if root[param_name] || instance_variable_get(varname).nil?
+          instance_variable_set(varname, root[param_name])
         end
-        unless instance_variable_defined?(varname)
-          $log.error "config error in:\n#{conf}"
-          raise ConfigError, "'#{name}' parameter is required"
-        end
-      }
+      end
     end
 
     CONFIG_TYPE_REGISTRY = Registry.new(:config_type, 'fluent/plugin/type_')
@@ -44,6 +57,96 @@ module Fluent
     end
 
     module ClassMethods
+      def configure_proxy_map
+        map = {}
+        self.define_singleton_method(:configure_proxy_map){ map }
+      end
+
+      def configure_proxy(mod_name)
+        unless configure_proxy_map[mod_name]
+          proxy = ConfigureProxy.new(mod_name, required: true, multi: false)
+          configure_proxy_map[mod_name] = proxy
+        end
+        configure_proxy_map[mod_name]
+      end
+
+      def config_param(name, *args, &block)
+        configure_proxy(self.name).config_param(name, *args, &block)
+        attr_accessor name
+      end
+
+      def config_set_default(name, defval)
+        configure_proxy(self.name).config_set_default(name, defval)
+      end
+
+      def config_section(name, *args, &block)
+        configure_proxy(self.name).config_section(name, *args, &block)
+        attr_accessor configure_proxy(self.name).sections[name].param_name
+      end
+
+      def merged_configure_proxy
+        configurables = ancestors.reverse.select{|a| a.respond_to?(:configure_proxy) }
+
+        # 'a.object_id.to_s' is to support anonymous class
+        #   which created in tests to overwrite original behavior temporally
+        #
+        # p Module.new.name   #=> nil
+        # p Class.new.name    #=> nil
+        # p AnyGreatClass.dup.name #=> nil
+        configurables.map{|a| a.configure_proxy(a.name || a.object_id.to_s) }.reduce(:merge)
+      end
+    end
+
+    class ConfigureProxy
+      attr_accessor :name, :param_name, :required, :multi, :params, :defaults, :sections
+      # config_param :desc, :string, :default => '....'
+      # config_set_default :buffer_type, :memory
+      #
+      # config_section :default, required: true, multi: false do
+      #   config_param :required, :bool, default: false
+      #   config_param :name, :string
+      #   config_param :power, :integer
+      # end
+      #
+      # config_section :child, param_name: 'children', required: false, multi: true do
+      #   config_param :name, :string
+      #   config_param :power, :integer, default: nil
+      #   config_section :item do
+      #     config_param :name
+      #   end
+      # end
+
+      def initialize(name, opts={})
+        @name = name.to_sym
+
+        @param_name = (opts[:param_name] || @name).to_sym
+        @required = opts[:required]
+        @multi = opts[:multi]
+
+        @params = {}
+        @defaults = {}
+        @sections = {}
+      end
+
+      def required? ; @required.nil? ? false : @required ; end
+
+      def multi? ; @multi.nil? ? true : @multi ; end
+
+      def merge(other) # self is base class, other is subclass
+        options = {
+          param_name: other.param_name,
+          required: ( other.required.nil? ? self.required : other.required ),
+          multi: ( other.multi.nil? ? self.multi : other.multi)
+        }
+        merged = self.class.new(other.name, options)
+
+        merged.params = self.params.merge(other.params)
+        merged.defaults = self.defaults.merge(other.defaults)
+        merged.sections = self.sections.merge(other.sections)
+
+        merged
+      end
+
       def config_param(name, *args, &block)
         name = name.to_sym
 
@@ -60,7 +163,7 @@ module Fluent
 
         type = opts[:type]
         if block && type
-          raise ArgumentError, "wrong number of arguments (#{1 + args.length} for #{block ? 2 : 3})"
+          raise ArgumentError, "wrong number of arguments (#{1 + args.length} for 2)" # block exists
         end
 
         begin
@@ -71,62 +174,45 @@ module Fluent
           raise ArgumentError, "unknown config_param type `#{type}'"
         end
 
-        params = config_params_set
-        params.delete(name)
-        params[name] = [block, opts]
+        @params.delete(name)
+        @sections.delete(name)
+        @params[name] = [block, opts]
 
         if opts.has_key?(:default)
           config_set_default(name, opts[:default])
         end
 
-        attr_accessor name
+        name
       end
 
       def config_set_default(name, defval)
         name = name.to_sym
 
-        defaults = config_defaults_set
-        defaults.delete(name)
-        defaults[name] = defval
+        @defaults.delete(name)
+        @defaults[name] = defval
 
         nil
       end
 
-      def config_params
-        singleton_value(:_config_params)
-      end
-
-      def config_defaults
-        singleton_value(:_config_defaults)
-      end
-
-      private
-      def config_params_set
-        singleton_value_set(:_config_params)
-      end
-
-      def config_defaults_set
-        singleton_value_set(:_config_defaults)
-      end
-
-      def singleton_value_set(name)
-        if methods(false).include?(name)
-          __send__(name)
-        else
-          val = {}
-          define_singleton_method(name) { val }
-          val
+      def config_section(name, *args, &block)
+        unless block_given?
+          raise ArgumentError, "config_section requires block parameter"
         end
-      end
+        name = name.to_sym
 
-      def singleton_value(name)
-        val = {}
-        ancestors.reverse_each { |c|
-          if c.methods(false).include?(name)
-            val.merge!(c.__send__(name))
-          end
-        }
-        val
+        opts = {}
+        unless args.empty? || args.size == 1 && args.first.is_a?(Hash)
+          raise ArgumentError, "unknown config_section arguments: #{args.inspect}"
+        end
+
+        sub_proxy = ConfigureProxy.new(name, *args)
+        sub_proxy.instance_exec(&block)
+
+        @params.delete(name)
+        @sections.delete(name)
+        @sections[name] = sub_proxy
+
+        name
       end
     end
   end
