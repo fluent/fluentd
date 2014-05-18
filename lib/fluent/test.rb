5a03406b2bf898975866cbad410f71c3bf01b6f0
sonots <sonots@gmail.com>
2014-05-18 12:20:28 +0900

    Use DummyLogDevice

--- lib/fluent/test.rb
+++ lib/fluent/test.rb
@@ -4,4 +4,4 @@ require 'fluent/test/base'
 require 'fluent/test/input_test'
 require 'fluent/test/output_test'
 
-$log ||= Fluent::Log.new
+$log ||= Fluent::Log.new(Fluent::Test::DummyLogDevice.new, Fluent::Log::LEVEL_WARN)
