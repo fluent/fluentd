6401aa6d58b16124331e2b8fa392b8138d648edd
Masahiro Nakagawa <repeatedly@gmail.com>
2014-05-29 14:50:04 +0900

    Improve gzipped file check in out_file test

--- test/plugin/test_out_file.rb
+++ test/plugin/test_out_file.rb
@@ -61,23 +61,42 @@ class FileOutputTest < Test::Unit::TestCase
                  data
   end
 
+  def check_gzipped_result(path, expect)
+    # Zlib::GzipReader has a bug of concatenated file: https://bugs.ruby-lang.org/issues/9790
+    # Following code from https://www.ruby-forum.com/topic/971591#979520
+    result = ''
+    File.open(path) { |io|
+      loop do
+        gzr = Zlib::GzipReader.new(io)
+        result << gzr.read
+        unused = gzr.unused
+        gzr.finish
+        break if unused.nil?
+        io.pos -= unused.length
+      end
+    }
+
+    assert_equal expect, result
+  end
+
   def test_write_path_increment
     d = create_driver
 
     time = Time.parse("2011-01-02 13:14:15 UTC").to_i
     d.emit({"a"=>1}, time)
     d.emit({"a"=>2}, time)
+    formatted_lines = %[2011-01-02T13:14:15Z\ttest\t{"a":1}\n] + %[2011-01-02T13:14:15Z\ttest\t{"a":2}\n]
 
     # FileOutput#write returns path
     path = d.run
     assert_equal "#{TMP_DIR}/out_file_test._0.log.gz", path
-    size = File::Stat.new("#{TMP_DIR}/out_file_test._0.log.gz").size
+    check_gzipped_result(path, formatted_lines)
     path = d.run
     assert_equal "#{TMP_DIR}/out_file_test._1.log.gz", path
-    assert_equal size, File::Stat.new("#{TMP_DIR}/out_file_test._1.log.gz").size
+    check_gzipped_result(path, formatted_lines)
     path = d.run
     assert_equal "#{TMP_DIR}/out_file_test._2.log.gz", path
-    assert_equal size, File::Stat.new("#{TMP_DIR}/out_file_test._2.log.gz").size
+    check_gzipped_result(path, formatted_lines)
   end
 
   def test_write_without_path_increment
@@ -91,18 +110,18 @@ class FileOutputTest < Test::Unit::TestCase
     time = Time.parse("2011-01-02 13:14:15 UTC").to_i
     d.emit({"a"=>1}, time)
     d.emit({"a"=>2}, time)
+    formatted_lines = %[2011-01-02T13:14:15Z\ttest\t{"a":1}\n] + %[2011-01-02T13:14:15Z\ttest\t{"a":2}\n]
 
     # FileOutput#write returns path
     path = d.run
     assert_equal "#{TMP_DIR}/out_file_test..log.gz", path
-    size = File::Stat.new("#{TMP_DIR}/out_file_test..log.gz").size
+    check_gzipped_result(path, formatted_lines)
     path = d.run
     assert_equal "#{TMP_DIR}/out_file_test..log.gz", path
-    assert size < File::Stat.new("#{TMP_DIR}/out_file_test..log.gz").size
-    size = File::Stat.new("#{TMP_DIR}/out_file_test..log.gz").size
+    check_gzipped_result(path, formatted_lines * 2)
     path = d.run
     assert_equal "#{TMP_DIR}/out_file_test..log.gz", path
-    assert size < File::Stat.new("#{TMP_DIR}/out_file_test..log.gz").size
+    check_gzipped_result(path, formatted_lines * 3)
   end
 
   def test_write_with_symlink
