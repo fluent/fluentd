bad0322f35a918ccbbd5ce84f4a7e687ef70cc4a
sonots <sonots@gmail.com>
2014-10-30 02:37:15 +0900

    add filter_record_transformer plugin

---
+++ lib/fluent/plugin/filter_record_transformer.rb
@@ -0,0 +1,183 @@
+require 'ostruct'
+
+module Fluent
+  class RecordTransformerFilter < Filter
+    Fluent::Plugin.register_filter('record_transformer', self)
+
+    def initialize
+      require 'socket'
+      super
+    end
+
+    config_param :remove_keys, :string, :default => nil
+    config_param :keep_keys, :string, :default => nil
+    config_param :renew_record, :bool, :default => false
+    config_param :enable_ruby, :bool, :default => false
+
+    def configure(conf)
+      super
+
+      @map = {}
+      # <record></record> directive
+      conf.elements.select { |element| element.name == 'record' }.each do |element|
+        element.each_pair do |k, v|
+          element.has_key?(k) # to suppress unread configuration warning
+          @map[k] = v
+        end
+      end
+
+      if @remove_keys
+        @remove_keys = @remove_keys.split(',')
+      end
+
+      if @keep_keys
+        raise Fluent::ConfigError, "`renew_record` must be true to use `keep_keys`" unless @renew_record
+        @keep_keys = @keep_keys.split(',')
+      end
+
+      @placeholder_expander =
+        if @enable_ruby
+          # require utilities which would be used in ruby placeholders
+          require 'pathname'
+          require 'uri'
+          require 'cgi'
+          RubyPlaceholderExpander.new(log)
+        else
+          PlaceholderExpander.new(log)
+        end
+
+      @hostname = Socket.gethostname
+    end
+
+    def filter_stream(tag, es)
+      new_es = MultiEventStream.new
+      tag_parts = tag.split('.')
+      tag_prefix = tag_prefix(tag_parts)
+      tag_suffix = tag_suffix(tag_parts)
+      placeholders = {
+        'tag' => tag,
+        'tag_parts' => tag_parts,
+        'tag_prefix' => tag_prefix,
+        'tag_suffix' => tag_suffix,
+        'hostname' => @hostname,
+      }
+      last_record = nil
+      es.each do |time, record|
+        last_record = record # for debug log
+        new_record = reform(time, record, placeholders)
+        new_es.add(time, new_record)
+      end
+      new_es
+    rescue => e
+      log.warn "failed to reform records", :error_class => e.class, :error => e.message
+      log.warn_backtrace
+      log.debug "map:#{@map} record:#{last_record} placeholders:#{placeholders}"
+    end
+
+    private
+
+    def reform(time, record, opts)
+      @placeholder_expander.prepare_placeholders(time, record, opts)
+
+      new_record = @renew_record ? {} : record.dup
+      @keep_keys.each {|k| new_record[k] = record[k]} if @keep_keys and @renew_record
+      @map.each_pair {|k, v| new_record[k] = @placeholder_expander.expand(v) }
+      @remove_keys.each {|k| new_record.delete(k) } if @remove_keys
+
+      new_record
+    end
+
+    def tag_prefix(tag_parts)
+      return [] if tag_parts.empty?
+      tag_prefix = [tag_parts.first]
+      1.upto(tag_parts.size-1).each do |i|
+        tag_prefix[i] = "#{tag_prefix[i-1]}.#{tag_parts[i]}"
+      end
+      tag_prefix
+    end
+
+    def tag_suffix(tag_parts)
+      return [] if tag_parts.empty?
+      rev_tag_parts = tag_parts.reverse
+      rev_tag_suffix = [rev_tag_parts.first]
+      1.upto(tag_parts.size-1).each do |i|
+        rev_tag_suffix[i] = "#{rev_tag_parts[i]}.#{rev_tag_suffix[i-1]}"
+      end
+      rev_tag_suffix.reverse!
+    end
+
+    class PlaceholderExpander
+      attr_reader :placeholders, :log
+
+      def initialize(log)
+        @log = log
+      end
+
+      def prepare_placeholders(time, record, opts)
+        placeholders = { '${time}' => Time.at(time).to_s }
+        record.each {|key, value| placeholders.store("${#{key}}", value) }
+
+        opts.each do |key, value|
+          if value.kind_of?(Array) # tag_parts, etc
+            size = value.size
+            value.each_with_index { |v, idx|
+              placeholders.store("${#{key}[#{idx}]}", v)
+              placeholders.store("${#{key}[#{idx-size}]}", v) # support [-1]
+            }
+          else # string, interger, float, and others?
+            placeholders.store("${#{key}}", value)
+          end
+        end
+
+        @placeholders = placeholders
+      end
+
+      def expand(str)
+        str.gsub(/(\${[a-zA-Z0-9_]+(\[-?[0-9]+\])?}|__[A-Z_]+__)/) {
+          log.warn "unknown placeholder `#{$1}` found" unless @placeholders.include?($1)
+          @placeholders[$1]
+        }
+      end
+    end
+
+    class RubyPlaceholderExpander
+      attr_reader :placeholders, :log
+
+      def initialize(log)
+        @log = log
+      end
+
+      # Get placeholders as a struct
+      #
+      # @param [Time]   time        the time
+      # @param [Hash]   record      the record
+      # @param [Hash]   opts        others
+      def prepare_placeholders(time, record, opts)
+        struct = UndefOpenStruct.new(record)
+        struct.time = Time.at(time)
+        opts.each {|key, value| struct.__send__("#{key}=", value) }
+        @placeholders = struct
+      end
+
+      # Replace placeholders in a string
+      #
+      # @param [String] str         the string to be replaced
+      def expand(str)
+        interpolated = str.gsub(/\$\{([^}]+)\}/, '#{\1}') # ${..} => #{..}
+        begin
+          eval "\"#{interpolated}\"", @placeholders.instance_eval { binding }
+        rescue => e
+          log.warn "failed to expand `#{str}`", :error_class => e.class, :error => e.message
+          log.warn_backtrace
+          nil
+        end
+      end
+
+      class UndefOpenStruct < OpenStruct
+        (Object.instance_methods).each do |m|
+          undef_method m unless m.to_s =~ /^__|respond_to_missing\?|object_id|public_methods|instance_eval|method_missing|define_singleton_method|respond_to\?|new_ostruct_member/
+        end
+      end
+    end
+  end
+end
---
+++ test/plugin/test_filter_record_transformer.rb
@@ -0,0 +1,251 @@
+require_relative '../helper'
+require 'timecop'
+require 'fluent/test'
+require 'fluent/plugin/filter_record_transformer'
+
+# TODO: Replace with FilterTestDriver
+class RecordTransformerFilterTest < Test::Unit::TestCase
+  setup do
+    @instance = Fluent::RecordTransformerFilter.new
+    @hostname = Socket.gethostname.chomp
+    @tag = 'test.tag'
+    @tag_parts = @tag.split('.')
+    @time = Time.utc(1,2,3,4,5,2010,nil,nil,nil,nil)
+    Timecop.freeze(@time)
+  end
+
+  teardown do
+    Timecop.return
+  end
+
+  def configure(instance, conf, use_v1 = false)
+    config = if conf.is_a?(Fluent::Config::Element)
+               str
+             else
+               Fluent::Config.parse(conf, "(test)", "(test_dir)", use_v1)
+             end
+    instance.configure(config)
+    instance
+  end
+
+  sub_test_case 'configure' do
+    test 'check default' do
+      assert_nothing_raised do
+        configure(@instance, '')
+      end
+    end
+
+    test "keep_keys must be specified together with renew_record true" do
+      assert_raise(Fluent::ConfigError) do
+        configure(@instance, %[keep_keys a])
+      end
+    end
+  end
+
+  sub_test_case "test options" do
+    def emit(config, msgs = [''])
+      es = Fluent::MultiEventStream.new
+      msgs.each do |msg|
+        es.add(@time, {'foo' => 'bar', 'message' => msg})
+      end
+
+      configure(@instance, config)
+      @instance.filter_stream(@tag, es)
+    end
+
+    CONFIG = %[
+      <record>
+        hostname ${hostname}
+        tag ${tag}
+        time ${time}
+        message ${hostname} ${tag_parts[-1]} ${message}
+      </record>
+    ]
+
+    test 'typical usage' do
+      msgs = ['1', '2']
+      es = emit(CONFIG, msgs)
+      es.each_with_index do |(t, r), i|
+        assert_equal('bar', r['foo'])
+        assert_equal(@hostname, r['hostname'])
+        assert_equal(@tag, r['tag'])
+        assert_equal(@time.to_s, r['time'])
+        assert_equal("#{@hostname} #{@tag_parts[-1]} #{msgs[i]}", r['message'])
+      end
+    end
+
+    test 'remove_keys' do
+      config = CONFIG + %[remove_keys foo,message]
+      es = emit(config)
+      es.each_with_index do |(t, r), i|
+        assert_not_include(r, 'foo')
+        assert_equal(@hostname, r['hostname'])
+        assert_equal(@tag, r['tag'])
+        assert_equal(@time.to_s, r['time'])
+        assert_not_include(r, 'message')
+      end
+    end
+
+    test 'renew_record' do
+      config = CONFIG + %[renew_record true]
+      msgs = ['1', '2']
+      es = emit(config, msgs)
+      es.each_with_index do |(t, r), i|
+        assert_not_include(r, 'foo')
+        assert_equal(@hostname, r['hostname'])
+        assert_equal(@tag, r['tag'])
+        assert_equal(@time.to_s, r['time'])
+        assert_equal("#{@hostname} #{@tag_parts[-1]} #{msgs[i]}", r['message'])
+      end
+    end
+
+    test 'keep_keys' do
+      config = %[renew_record true\nkeep_keys foo,message]
+      msgs = ['1', '2']
+      es = emit(config, msgs)
+      es.each_with_index do |(t, r), i|
+        assert_equal('bar', r['foo'])
+        assert_equal(msgs[i], r['message'])
+      end
+    end
+
+    test 'enable_ruby' do
+      config = %[
+        enable_ruby yes
+        <record>
+          message ${hostname} ${tag_parts.last} ${URI.encode(message)}
+        </record>
+      ]
+      msgs = ['1', '2']
+      es = emit(config, msgs)
+      es.each_with_index do |(t, r), i|
+        assert_equal("#{@hostname} #{@tag_parts[-1]} #{msgs[i]}", r['message'])
+      end
+    end
+  end
+
+  sub_test_case 'test placeholders' do
+    def emit(config, msgs = [''])
+      es = Fluent::MultiEventStream.new
+      msgs.each do |msg|
+        es.add(@time, {'eventType0' => 'bar', 'message' => msg})
+      end
+
+      configure(@instance, config)
+      @instance.filter_stream(@tag, es)
+    end
+
+    %w[yes no].each do |enable_ruby|
+      test "hostname with enble_ruby #{enable_ruby}" do
+        config = %[
+          enable_ruby #{enable_ruby}
+          <record>
+            message ${hostname}
+          </record>
+        ]
+        es = emit(config)
+        es.each do |t, r|
+          assert_equal(@hostname, r['message'])
+        end
+      end
+
+      test "tag with enable_ruby #{enable_ruby}" do
+        config = %[
+          enable_ruby #{enable_ruby}
+          <record>
+            message ${tag}
+          </record>
+        ]
+        es = emit(config)
+        es.each do |t, r|
+          assert_equal(@tag, r['message'])
+        end
+      end
+
+      test "tag_parts with enable_ruby #{enable_ruby}" do
+        config = %[
+          enable_ruby #{enable_ruby}
+          <record>
+            message ${tag_parts[0]} ${tag_parts[-1]}
+          </record>
+        ]
+        expected = "#{@tag.split('.').first} #{@tag.split('.').last}"
+        es = emit(config)
+        es.each do |t, r|
+          assert_equal(expected, r['message'])
+        end
+      end
+
+      test "${tag_prefix[N]} and ${tag_suffix[N]} with enable_ruby #{enable_ruby}" do
+        config = %[
+          enable_ruby #{enable_ruby}
+          <record>
+            message ${tag_prefix[1]} ${tag_prefix[-2]} ${tag_suffix[2]} ${tag_suffix[-3]}
+          </record>
+        ]
+        @tag = 'prefix.test.tag.suffix'
+        expected = "prefix.test prefix.test.tag tag.suffix test.tag.suffix"
+        es = emit(config)
+        es.each do |t, r|
+          assert_equal(expected, r['message'])
+        end
+      end
+
+      test "time with enable_ruby #{enable_ruby}" do
+        config = %[
+          enable_ruby #{enable_ruby}
+          <record>
+            message ${time}
+          </record>
+        ]
+        es = emit(config)
+        es.each do |t, r|
+          assert_equal(@time.to_s, r['message'])
+        end
+      end
+
+      test "record keys with enable_ruby #{enable_ruby}" do
+        config = %[
+          enable_ruby #{enable_ruby}
+          remove_keys eventType0
+          <record>
+            message bar ${message}
+            eventtype ${eventType0}
+          </record>
+        ]
+        msgs = ['1', '2']
+        es = emit(config, msgs)
+        es.each_with_index do |(t, r), i|
+          assert_not_include(r, 'eventType0')
+          assert_equal("bar", r['eventtype'])
+          assert_equal("bar #{msgs[i]}", r['message'])
+        end
+      end
+    end
+
+    test 'unknown placeholder (enable_ruby no)' do
+      config = %[
+        enable_ruby no
+        <record>
+          message ${unknown}
+        </record>
+      ]
+      mock(@instance.log).warn("unknown placeholder `${unknown}` found")
+      emit(config)
+    end
+
+    test 'failed to expand (enable_ruby yes)' do
+      config = %[
+        enable_ruby yes
+        <record>
+          message ${unknown['bar']}
+        </record>
+      ]
+      mock(@instance.log).warn("failed to expand `${unknown['bar']}`", anything)
+      es = emit(config)
+      es.each do |t, r|
+        assert_nil(r['message'])
+      end
+    end
+  end
+end
