08615241920a671effa6f0f102e6f04ac3468f19
Masahiro Nakagawa <repeatedly@gmail.com>
2014-11-11 01:22:18 -0800

    Improve Match test with more clear name

--- test/test_match.rb
+++ test/test_match.rb
@@ -5,84 +5,84 @@ class MatchTest < Test::Unit::TestCase
   include Fluent
 
   def test_simple
-    assert_match('a', 'a')
-    assert_match('a.b', 'a.b')
-    assert_not_match('a', 'b')
-    assert_not_match('a.b', 'aab')
+    assert_glob_match('a', 'a')
+    assert_glob_match('a.b', 'a.b')
+    assert_glob_not_match('a', 'b')
+    assert_glob_not_match('a.b', 'aab')
   end
 
   def test_wildcard
-    assert_match('a*', 'a')
-    assert_match('a*', 'ab')
-    assert_match('a*', 'abc')
+    assert_glob_match('a*', 'a')
+    assert_glob_match('a*', 'ab')
+    assert_glob_match('a*', 'abc')
 
-    assert_match('*a', 'a')
-    assert_match('*a', 'ba')
-    assert_match('*a', 'cba')
+    assert_glob_match('*a', 'a')
+    assert_glob_match('*a', 'ba')
+    assert_glob_match('*a', 'cba')
 
-    assert_match('*a*', 'a')
-    assert_match('*a*', 'ba')
-    assert_match('*a*', 'ac')
-    assert_match('*a*', 'bac')
+    assert_glob_match('*a*', 'a')
+    assert_glob_match('*a*', 'ba')
+    assert_glob_match('*a*', 'ac')
+    assert_glob_match('*a*', 'bac')
 
-    assert_not_match('a*', 'a.b')
-    assert_not_match('a*', 'ab.c')
-    assert_not_match('a*', 'ba')
-    assert_not_match('*a', 'ab')
+    assert_glob_not_match('a*', 'a.b')
+    assert_glob_not_match('a*', 'ab.c')
+    assert_glob_not_match('a*', 'ba')
+    assert_glob_not_match('*a', 'ab')
 
-    assert_match('a.*', 'a.b')
-    assert_match('a.*', 'a.c')
-    assert_not_match('a.*', 'ab')
+    assert_glob_match('a.*', 'a.b')
+    assert_glob_match('a.*', 'a.c')
+    assert_glob_not_match('a.*', 'ab')
 
-    assert_match('a.*.c', 'a.b.c')
-    assert_match('a.*.c', 'a.c.c')
-    assert_not_match('a.*.c', 'a.c')
+    assert_glob_match('a.*.c', 'a.b.c')
+    assert_glob_match('a.*.c', 'a.c.c')
+    assert_glob_not_match('a.*.c', 'a.c')
   end
 
   def test_recursive_wildcard
-    assert_match('a.**', 'a')
-    assert_not_match('a.**', 'ab')
-    assert_not_match('a.**', 'abc')
-    assert_match('a.**', 'a.b')
-    assert_not_match('a.**', 'ab.c')
-    assert_not_match('a.**', 'ab.d.e')
+    assert_glob_match('a.**', 'a')
+    assert_glob_not_match('a.**', 'ab')
+    assert_glob_not_match('a.**', 'abc')
+    assert_glob_match('a.**', 'a.b')
+    assert_glob_not_match('a.**', 'ab.c')
+    assert_glob_not_match('a.**', 'ab.d.e')
 
-    assert_match('a**', 'a')
-    assert_match('a**', 'ab')
-    assert_match('a**', 'abc')
-    assert_match('a**', 'a.b')
-    assert_match('a**', 'ab.c')
-    assert_match('a**', 'ab.d.e')
+    assert_glob_match('a**', 'a')
+    assert_glob_match('a**', 'ab')
+    assert_glob_match('a**', 'abc')
+    assert_glob_match('a**', 'a.b')
+    assert_glob_match('a**', 'ab.c')
+    assert_glob_match('a**', 'ab.d.e')
 
-    assert_match('**.a', 'a')
-    assert_not_match('**.a', 'ba')
-    assert_not_match('**.a', 'c.ba')
-    assert_match('**.a', 'b.a')
-    assert_match('**.a', 'cb.a')
-    assert_match('**.a', 'd.e.a')
+    assert_glob_match('**.a', 'a')
+    assert_glob_not_match('**.a', 'ba')
+    assert_glob_not_match('**.a', 'c.ba')
+    assert_glob_match('**.a', 'b.a')
+    assert_glob_match('**.a', 'cb.a')
+    assert_glob_match('**.a', 'd.e.a')
 
-    assert_match('**a', 'a')
-    assert_match('**a', 'ba')
-    assert_match('**a', 'c.ba')
-    assert_match('**a', 'b.a')
-    assert_match('**a', 'cb.a')
-    assert_match('**a', 'd.e.a')
+    assert_glob_match('**a', 'a')
+    assert_glob_match('**a', 'ba')
+    assert_glob_match('**a', 'c.ba')
+    assert_glob_match('**a', 'b.a')
+    assert_glob_match('**a', 'cb.a')
+    assert_glob_match('**a', 'd.e.a')
   end
 
   def test_or
-    assert_match('a.{b,c}', 'a.b')
-    assert_match('a.{b,c}', 'a.c')
-    assert_not_match('a.{b,c}', 'a.d')
+    assert_glob_match('a.{b,c}', 'a.b')
+    assert_glob_match('a.{b,c}', 'a.c')
+    assert_glob_not_match('a.{b,c}', 'a.d')
 
-    assert_match('a.{b,c}.**', 'a.b')
-    assert_match('a.{b,c}.**', 'a.c')
-    assert_not_match('a.{b,c}.**', 'a.d')
-    assert_not_match('a.{b,c}.**', 'a.cd')
+    assert_glob_match('a.{b,c}.**', 'a.b')
+    assert_glob_match('a.{b,c}.**', 'a.c')
+    assert_glob_not_match('a.{b,c}.**', 'a.d')
+    assert_glob_not_match('a.{b,c}.**', 'a.cd')
 
-    assert_match('a.{b.**,c}', 'a.b')
-    assert_match('a.{b.**,c}', 'a.b.c')
-    assert_match('a.{b.**,c}', 'a.c')
-    assert_not_match('a.{b.**,c}', 'a.c.d')
+    assert_glob_match('a.{b.**,c}', 'a.b')
+    assert_glob_match('a.{b.**,c}', 'a.b.c')
+    assert_glob_match('a.{b.**,c}', 'a.c')
+    assert_glob_not_match('a.{b.**,c}', 'a.c.d')
   end
 
   #def test_character_class
@@ -101,14 +101,13 @@ class MatchTest < Test::Unit::TestCase
   #  assert_not_match('[a-b0-9]', 'c')
   #end
 
-  def assert_match(pat, str)
+  def assert_glob_match(pat, str)
     m = GlobMatchPattern.new(pat)
-    assert_equal true, m.match(str)
+    assert_true m.match(str)
   end
 
-  def assert_not_match(pat, str)
+  def assert_glob_not_match(pat, str)
     m = GlobMatchPattern.new(pat)
-    assert_equal false, m.match(str)
+    assert_false m.match(str)
   end
 end
-
