e5480abec2b089256a7377e9a5b4bdacbaf72152
kayrus <kay.diam@gmail.com>
2014-11-20 12:35:26 +0100

    Added possibility to exclude labels and tag from the LTSV

--- lib/fluent/formatter.rb
+++ lib/fluent/formatter.rb
@@ -109,12 +109,19 @@ module Fluent
 
       config_param :delimiter, :string, :default => "\t"
       config_param :label_delimiter, :string, :default =>  ":"
+      config_param :include_label, :bool, :default => true
+      config_param :output_tag, :bool, :default => true
 
       def format(tag, time, record)
         filter_record(tag, time, record)
         formatted = record.inject('') { |result, pair|
-          result << @delimiter if result.length.nonzero?
-          result << "#{pair.first}#{@label_delimiter}#{pair.last}"
+          if @output_tag == false && pair.first == 'tag'
+            result = ''
+          else
+            result << @delimiter if result.length.nonzero?
+            result << "#{pair.first}#{@label_delimiter}" if @include_label
+            result << "#{pair.last}"
+          end
         }
         formatted << "\n"
         formatted
