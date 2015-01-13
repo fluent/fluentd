3405a1672a360aedf2aa6032e9d94e89892d17f1
Masahiro Nakagawa <repeatedly@gmail.com>
2015-01-14 05:19:47 +0900

    Merge pull request #535 from sabottenda/without_http_version
    Fix Nginx and Apache2 format to parse access log without http-version

--- test/test_parser.rb
+++ test/test_parser.rb
@@ -199,24 +199,32 @@ module ParserTest
 
     def setup
       @parser = TextParser::ApacheParser.new
+      @expected = {
+        'user'    => nil,
+        'method'  => 'GET',
+        'code'    => 200,
+        'size'    => 777,
+        'host'    => '192.168.0.1',
+        'path'    => '/',
+        'referer' => nil,
+        'agent'   => 'Opera/12.0'
+      }
     end
 
     def test_parse
       @parser.parse('192.168.0.1 - - [28/Feb/2013:12:00:00 +0900] "GET / HTTP/1.1" 200 777 "-" "Opera/12.0"') { |time, record|
         assert_equal(str2time('28/Feb/2013:12:00:00 +0900', '%d/%b/%Y:%H:%M:%S %z'), time)
-        assert_equal({
-          'user'    => nil,
-          'method'  => 'GET',
-          'code'    => 200,
-          'size'    => 777,
-          'host'    => '192.168.0.1',
-          'path'    => '/',
-          'referer' => nil,
-          'agent'   => 'Opera/12.0'
-        }, record)
+        assert_equal(@expected, record)
       }
       assert_equal(TextParser::ApacheParser::REGEXP, @parser.patterns['format'])
       assert_equal(TextParser::ApacheParser::TIME_FORMAT, @parser.patterns['time_format'])
+    end
+
+    def test_parse_without_http_version
+      @parser.parse('192.168.0.1 - - [28/Feb/2013:12:00:00 +0900] "GET /" 200 777 "-" "Opera/12.0"') { |time, record|
+        assert_equal(str2time('28/Feb/2013:12:00:00 +0900', '%d/%b/%Y:%H:%M:%S %z'), time)
+        assert_equal(@expected, record)
+      }
     end
   end
 
@@ -352,6 +360,13 @@ module ParserTest
       @parser.parse('127.0.0.1 192.168.0.1 - [28/Feb/2013:12:00:00 +0900] "GET /a[ ]b HTTP/1.1" 200 777 "-" "Opera/12.0"') { |time, record|
         assert_equal(str2time('28/Feb/2013:12:00:00 +0900', '%d/%b/%Y:%H:%M:%S %z'), time)
         assert_equal(@expected.merge('path' => '/a[ ]b'), record)
+      }
+    end
+
+    def test_parse_without_http_version
+      @parser.parse('127.0.0.1 192.168.0.1 - [28/Feb/2013:12:00:00 +0900] "GET /" 200 777 "-" "Opera/12.0"') { |time, record|
+        assert_equal(str2time('28/Feb/2013:12:00:00 +0900', '%d/%b/%Y:%H:%M:%S %z'), time)
+        assert_equal(@expected, record)
       }
     end
   end
