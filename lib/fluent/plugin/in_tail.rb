d8452b1a60c99c61da0e43c0b210de534b7c1644
Andrey Tikhonov <atikhonov@avito.ru>
2015-01-28 21:59:00 +0300

    Enable multiline mode if format contains 'multiline'

--- lib/fluent/plugin/in_tail.rb
+++ lib/fluent/plugin/in_tail.rb
@@ -49,7 +49,7 @@ module Fluent
       configure_parser(conf)
       configure_tag
 
-      @multiline_mode = conf['format'] == 'multiline'
+      @multiline_mode = conf['format'] =~ /multiline/
       @receive_handler = if @multiline_mode
                            method(:parse_multilines)
                          else
