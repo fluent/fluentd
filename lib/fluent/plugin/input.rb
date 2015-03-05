1aae6042cea9e68e91d55535021e2cf5a5e34d34
TAGOMORI Satoshi <tagomoris@gmail.com>
2015-03-05 15:15:02 +0900

    Add Fluent::Plugin::Base  * to reduce duplicated implementation in Input and Output

--- lib/fluent/plugin/input.rb
+++ lib/fluent/plugin/input.rb
@@ -14,39 +14,25 @@
 #    limitations under the License.
 #
 
-require 'fluent/plugin'
-require 'fluent/configurable'
-require 'fluent/config' # for PluginId
-require 'fluent/log'
+require 'fluent/plugin/base'
 
 module Fluent
   module Plugin
-    class Input
-      include Configurable
-      include PluginId
-      include PluginLoggerMixin
-
-      attr_accessor :router
-
+    class Input < Base
       def initialize
         super
       end
 
       def configure(conf)
         super
-
-        if label_name = conf['@label']
-          label = Engine.root_agent.find_label(label_name)
-          @router = label.event_router
-        elsif @router.nil?
-          @router = Engine.root_agent.event_router
-        end
       end
 
       def start
+        super
       end
 
       def shutdown
+        super
       end
     end
   end
