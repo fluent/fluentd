3d4c88faee15f96e4788e71c566edb771430dc13
TAGOMORI Satoshi <tagomoris@gmail.com>
2015-03-18 17:32:22 +0900

    fix mark Fluent::DEFAULT_LISTEN_PORT as obsolete (it is of in/out_forward)

--- lib/fluent/env.rb
+++ lib/fluent/env.rb
@@ -18,7 +18,7 @@ module Fluent
   DEFAULT_CONFIG_PATH = ENV['FLUENT_CONF'] || '/etc/fluent/fluent.conf'
   DEFAULT_PLUGIN_DIR = ENV['FLUENT_PLUGIN'] || '/etc/fluent/plugin'
   DEFAULT_SOCKET_PATH = ENV['FLUENT_SOCKET'] || '/var/run/fluent/fluent.sock'
-  DEFAULT_LISTEN_PORT = 24224
+  DEFAULT_LISTEN_PORT = 24224 # TODO: obsolete
   DEFAULT_FILE_PERMISSION = 0644
   DEFAULT_DIR_PERMISSION = 0755
 end
