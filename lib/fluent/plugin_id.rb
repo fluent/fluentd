39d79ba6e148362796322d4db1ef452ffd7265fa
TAGOMORI Satoshi <tagomoris@gmail.com>
2015-03-18 17:29:51 +0900

    Separate PluginId mixin from config.rb, which is above understanding

---
+++ lib/fluent/plugin_id.rb
@@ -0,0 +1,31 @@
+#
+# Fluent
+#
+#    Licensed under the Apache License, Version 2.0 (the "License");
+#    you may not use this file except in compliance with the License.
+#    You may obtain a copy of the License at
+#
+#        http://www.apache.org/licenses/LICENSE-2.0
+#
+#    Unless required by applicable law or agreed to in writing, software
+#    distributed under the License is distributed on an "AS IS" BASIS,
+#    WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+#    See the License for the specific language governing permissions and
+#    limitations under the License.
+#
+
+require 'fluent/configurable'
+
+module Fluent
+  module PluginId
+    def configure(conf)
+      @id = conf['@id'] || conf['id']
+      @_id_configured = !!@id # plugin id is explicitly configured by users (or not)
+      super
+    end
+
+    def plugin_id
+      @id ? @id : "object:#{object_id.to_s(16)}"
+    end
+  end
+end
