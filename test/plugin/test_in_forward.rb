14056cf3eb7d39af708a8dff4bdc09b4349493f2
TAGOMORI Satoshi <tagomoris@gmail.com>
2015-03-31 12:26:17 +0900

    fix to flush data on testing write socket

--- test/plugin/test_in_forward.rb
+++ test/plugin/test_in_forward.rb
@@ -802,6 +802,7 @@ class ForwardInputTest < Test::Unit::TestCase
       ping.push('', '')
     end
     io.write ping.to_msgpack
+    io.flush
 
     # reading pong
     pong_data = read_data(io, auth_response_timeout)
@@ -826,6 +827,7 @@ class ForwardInputTest < Test::Unit::TestCase
     res = nil
     p({send: data}) if $json_ssl
     io.write data
+    io.flush
     if try_to_receive_response
       @responses << read_data(io, response_timeout)
     end
