702a154698acd70cc168ab37a99287708c48d513
Shun Takebayashi <shun@takebayashi.asia>
2015-06-13 11:40:59 +0900

    Make output_type of out_stdout accept formatters

--- lib/fluent/plugin/out_stdout.rb
+++ lib/fluent/plugin/out_stdout.rb
@@ -18,30 +18,17 @@ module Fluent
   class StdoutOutput < Output
     Plugin.register_output('stdout', self)
 
-    OUTPUT_PROCS = {
-      :json => Proc.new {|record| Yajl.dump(record) },
-      :hash => Proc.new {|record| record.to_s },
-    }
-
-    config_param :output_type, :default => :json do |val|
-      case val.downcase
-      when 'json'
-        :json
-      when 'hash'
-        :hash
-      else
-        raise ConfigError, "stdout output output_type should be 'json' or 'hash'"
-      end
-    end
+    config_param :output_type, :default => 'json'
 
     def configure(conf)
       super
-      @output_proc = OUTPUT_PROCS[@output_type]
+      @formatter = Plugin.new_formatter(@output_type)
+      @formatter.configure(conf)
     end
 
     def emit(tag, es, chain)
       es.each {|time,record|
-        $log.write "#{Time.at(time).localtime} #{tag}: #{@output_proc.call(record)}\n"
+        $log.write "#{Time.at(time).localtime} #{tag}: #{@formatter.format(tag, time, record).chomp}\n"
       }
       $log.flush
 
