fc834ab87a09a1dc452999afc6d169fd854acad8
Yuki Ito <yuki@gnnk.net>
2015-08-06 14:02:40 +0900

    Separate Fluent::NTime declaration

--- lib/fluent/ntime.rb
+++ lib/fluent/ntime.rb
@@ -1,53 +1,55 @@
-class Fluent::NTime
-  def initialize(sec, nsec = 0)
-    @sec = sec
-    @nsec = nsec
-  end
-
-  def ==(other)
-    if other.is_a?(Fluent::NTime)
-      @sec == other.sec
-    else
-      @sec == other
+module Fluent
+  class NTime
+    def initialize(sec, nsec = 0)
+      @sec = sec
+      @nsec = nsec
     end
-  end
 
-  def sec
-    @sec
-  end
+    def ==(other)
+      if other.is_a?(Fluent::NTime)
+        @sec == other.sec
+      else
+        @sec == other
+      end
+    end
 
-  def nsec
-    @nsec
-  end
+    def sec
+      @sec
+    end
 
-  def to_int
-    @sec
-  end
+    def nsec
+      @nsec
+    end
 
-  # for Time.at
-  def to_r
-    Rational(@sec * 1_000_000_000 + @nsec, 1_000_000_000)
-  end
+    def to_int
+      @sec
+    end
 
-  # for > and others
-  def coerce(other)
-    [other, @sec]
-  end
+    # for Time.at
+    def to_r
+      Rational(@sec * 1_000_000_000 + @nsec, 1_000_000_000)
+    end
 
-  def to_s
-    @sec.to_s
-  end
+    # for > and others
+    def coerce(other)
+      [other, @sec]
+    end
 
-  def self.from_time(time)
-    Fluent::NTime.new(time.to_i, time.nsec)
-  end
+    def to_s
+      @sec.to_s
+    end
 
-  def self.now
-    from_time(Time.now)
-  end
+    def self.from_time(time)
+      Fluent::NTime.new(time.to_i, time.nsec)
+    end
 
-  ## TODO: For performance, implement +, -, and so on
-  def method_missing(name, *args, &block)
-    @sec.send(name, *args, &block)
+    def self.now
+      from_time(Time.now)
+    end
+
+    ## TODO: For performance, implement +, -, and so on
+    def method_missing(name, *args, &block)
+      @sec.send(name, *args, &block)
+    end
   end
 end
