2bf623525d3e61cd1bbc00d6c3ddb3c138c6721e
Naotoshi Seo <sonots@gmail.com>
2015-11-28 01:21:29 +0900

    Merge pull request #721 from okkez/add-descriptions-base-input
    Add descriptions to Fluent::SocketUtil::BaseInput

--- lib/fluent/plugin/socket_util.rb
+++ lib/fluent/plugin/socket_util.rb
@@ -94,10 +94,15 @@ module Fluent
         require 'fluent/parser'
       end
 
+      desc 'Tag of output events.'
       config_param :tag, :string
+      desc 'The format of the payload.'
       config_param :format, :string
+      desc 'The port to listen to.'
       config_param :port, :integer, :default => 5150
+      desc 'The bind address to listen to.'
       config_param :bind, :string, :default => '0.0.0.0'
+      desc "The field name of the client's hostname."
       config_param :source_host_key, :string, :default => nil
       config_param :blocking_timeout, :time, :default => 0.5
 
