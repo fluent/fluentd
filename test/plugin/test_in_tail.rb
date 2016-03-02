41d17240852d0dff8ba811eff1f6c7a501c58af6
Masahiro Nakagawa <repeatedly@gmail.com>
2016-03-02 22:02:38 +0900

    Merge pull request #810 from fluent/fix-in_tail-for-bqle
    Fix in_tail input messages loss when receive_lines fail. fix #699

--- test/plugin/test_in_tail.rb
+++ test/plugin/test_in_tail.rb
@@ -638,4 +638,46 @@ class TailInputTest < Test::Unit::TestCase
       assert_equal({"message" => "test4"}, emits[1][2])
     end
   end
+
+  sub_test_case 'emit error cases' do
+    def test_emit_error_with_buffer_queue_limit_error
+      emits = execute_test(::Fluent::BufferQueueLimitError, "queue size exceeds limit")
+      assert_equal(10, emits.length)
+      10.times { |i|
+        assert_equal({"message" => "test#{i}"}, emits[i][2])
+      }
+    end
+
+    def test_emit_error_with_non_buffer_queue_limit_error
+      emits = execute_test(StandardError, "non BufferQueueLimitError error")
+      assert_true(emits.size > 0 && emits.size != 10)
+      emits.size.times { |i|
+        assert_equal({"message" => "test#{10 - emits.size + i}"}, emits[i][2])
+      }
+    end
+
+    def execute_test(error_class, error_message)
+      d = create_driver(CONFIG_READ_FROM_HEAD + SINGLE_LINE_CONFIG)
+      # Use define_singleton_method instead of d.emit_stream to capture local variable
+      d.define_singleton_method(:emit_stream) do |tag, es|
+        @test_num_errors ||= 0
+        if @test_num_errors < 5
+          @test_num_errors += 1
+          raise error_class, error_message
+        else
+          @emit_streams << [tag, es.to_a]
+        end
+      end
+
+      d.run do
+        10.times { |i|
+          File.open("#{TMP_DIR}/tail.txt", "ab") { |f| f.puts "test#{i}" }
+          sleep 0.5
+        }
+        sleep 1
+      end
+
+      d.emits
+    end
+  end
 end
