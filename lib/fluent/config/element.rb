afa27c1d06617d993e1e7de4ec048ac972192cd8
Kenji Okimoto <okimoto@clear-code.com>
2016-03-25 14:05:03 +0900

    Set v1_config properly

--- lib/fluent/config/element.rb
+++ lib/fluent/config/element.rb
@@ -120,6 +120,7 @@ module Fluent
       def to_masked_element
         new_elems = @elements.map { |e| e.to_masked_element }
         new_elem = Element.new(@name, @arg, {}, new_elems, @unused)
+        new_elem.v1_config = @v1_config
         each_pair { |k, v|
           new_elem[k] = secret_param?(k) ? 'xxxxxx' : v
         }
