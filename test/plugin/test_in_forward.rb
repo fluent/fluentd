481dfa57f56e17ca924c3b026a673dc65fac9c9f
Kenji Okimoto <okimoto@clear-code.com>
2016-04-26 16:51:24 +0900

    Skip test on Windows for older Ruby properly
    `read_nonblock` in Windows was officially supported at Ruby 2.3, but it
    has existed in past ruby versions. So we cannot check it by
    `respond_to?(:read_nonblock)` even in older versions & Windows.

--- test/plugin/test_in_forward.rb
+++ test/plugin/test_in_forward.rb
@@ -857,7 +857,7 @@ class ForwardInputTest < Test::Unit::TestCase
   # '' : socket is disconnected without any data
   # nil: socket read timeout
   def read_data(io, timeout)
-    skip unless io.respond_to?(:read_nonblock)
+    skip if Fluent.windows? && RUBY_VERSION < "2.3.0"
     res = ''
     timeout_at = Time.now + timeout
     begin
