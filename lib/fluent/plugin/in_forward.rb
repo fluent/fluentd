768c1e49a48887c70ccbc438ff1ee0f4729f9ff0
Kenji Okimoto <okimoto@clear-code.com>
2016-05-11 11:17:23 +0900

    Add "_" prefix to unused local variable
    This change suppresses following warning:
    
    lib/fluent/plugin/in_forward.rb:232: warning: assigned but unused variable - proto

--- lib/fluent/plugin/in_forward.rb
+++ lib/fluent/plugin/in_forward.rb
@@ -229,7 +229,7 @@ module Fluent
         super(io)
 
         if io.is_a?(TCPSocket) # for unix domain socket support in the future
-          proto, port, host, addr = ( io.peeraddr rescue PEERADDR_FAILED )
+          _proto, port, host, addr = ( io.peeraddr rescue PEERADDR_FAILED )
           @source = "host: #{host}, addr: #{addr}, port: #{port}"
 
           opt = [1, linger_timeout].pack('I!I!')  # { int l_onoff; int l_linger; }
