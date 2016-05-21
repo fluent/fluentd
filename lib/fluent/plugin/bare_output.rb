af46b723b0eb1ef3a65963b349206dbc91d60e4b
TAGOMORI Satoshi <tagomoris@gmail.com>
2016-05-21 18:55:56 +0900

    add Fluent::Plugin::BareOutput to support v0.12 MultiOutput plugins

--- lib/fluent/plugin/bare_output.rb
+++ lib/fluent/plugin/bare_output.rb
@@ -15,24 +15,19 @@
 #
 
 require 'fluent/plugin/base'
+
 require 'fluent/log'
 require 'fluent/plugin_id'
 require 'fluent/plugin_helper'
 
 module Fluent
   module Plugin
-    class MultiOutput < Base
+    class BareOutput < Base
       include PluginId
       include PluginLoggerMixin
-      include PluginHelper::Mixin # for event_emitter
+      include PluginHelper::Mixin
 
-      helpers :event_emitter # to get router from agent, which will be supplied to child plugins
-
-      config_section :store, param_name: :stores, multi: true, required: true do
-        config_param :@type, :string, default: nil
-      end
-
-      attr_reader :outputs
+      attr_reader :num_errors, :emit_count, :emit_records
 
       def process(tag, es)
         raise NotImplementedError, "BUG: output plugins MUST implement this method"
@@ -40,44 +35,12 @@ module Fluent
 
       def initialize
         super
-        @outputs = []
-
-        @compat = false
-
         @counters_monitor = Monitor.new
         # TODO: well organized counters
         @num_errors = 0
         @emit_count = 0
         @emit_records = 0
-        # @write_count = 0
-        # @rollback_count = 0
       end
-
-      def configure(conf)
-        super
-
-        @stores.each do |store|
-          store_conf = store.corresponding_config_element
-          type = store_conf['@type']
-          unless type
-            raise Fluent::ConfigError, "Missing '@type' parameter in <store> section"
-          end
-
-          log.debug "adding store", type: type
-
-          output = Fluent::Plugin.new_output(type)
-          if output.has_router?
-            output.router = router
-          end
-          output.configure(store_conf)
-          @outputs << output
-        end
-      end
-
-      # Child plugin's lifecycles are controlled by agent automatically.
-      # It calls `outputs` to traverse plugins, and invoke start/stop/*shutdown/close/terminate on these directly.
-      # * `start` of this plugin will be called after child plugins
-      # * `stop`, `*shutdown`, `close` and `terminate` of this plugin will be called before child plugins
 
       def emit_sync(tag, es)
         @counters_monitor.synchronize{ @emit_count += 1 }
