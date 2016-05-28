7363a2b3071caceaf2db0d3f1c599f84588554f2
TAGOMORI Satoshi <tagomoris@gmail.com>
2016-05-28 16:35:46 +0900

    add tests to confirm subclass can override default value for subsection

--- test/config/test_configurable.rb
+++ test/config/test_configurable.rb
@@ -323,6 +323,12 @@ module ConfigurableSpec
       end
     end
 
+    class SubOwner < Owner
+      config_section :buffer do
+        config_set_default :size_of_something, 2048
+      end
+    end
+
     class FlatChild
       include Fluent::Configurable
       attr_accessor :owner
@@ -1031,6 +1037,14 @@ module Fluent::Config
         child.owner = owner
         child.configure(config_element('ROOT', '', {}, []))
         assert_equal 1024, child.size_of_something
+      end
+
+      test 'even in subclass of owner' do
+        owner = ConfigurableSpec::OverwriteDefaults::SubOwner.new
+        child = ConfigurableSpec::OverwriteDefaults::BufferChild.new
+        child.owner = owner
+        child.configure(config_element('ROOT', '', {}, []))
+        assert_equal 2048, child.size_of_something
       end
     end
 
