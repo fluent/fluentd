30b138b5190369c547f8cf40c03a87f277c455bd
TAGOMORI Satoshi <tagomoris@gmail.com>
2016-06-23 19:56:35 +0900

    roughly implement for parser/formatter and shrink code

--- lib/fluent/plugin_helper/compat_parameters.rb
+++ lib/fluent/plugin_helper/compat_parameters.rb
@@ -24,7 +24,7 @@ module Fluent
       # This helper is mainly to convert plugins from v0.12 API
       # to v0.14 API safely, without breaking user deployment.
 
-      BUFFER_PARAMS_MAP = {
+      BUFFER_PARAMS = {
         "buffer_type" => "@type",
         "buffer_path" => "path",
         "num_threads"                 => "flush_thread_count",
@@ -45,6 +45,45 @@ module Fluent
         "time_slice_wait" => "timekey_wait",
       }
 
+      PARSER_PARAMS = {
+        "format" => "@type",
+        "time_key"    => "time_key",
+        "time_format" => "time_format",
+        "delimiter"   => "delimiter",
+        "json_parser"      => "json_parser", # JSONParser
+        "label_delimiter"  => "label_delimiter", # LabeledTSVParser
+        "format_firstline" => "format_firstline", # MultilineParser
+        "message_key"      => "message_key", # NoneParser
+        "with_priority"    => "with_priority", # SyslogParser
+      }
+
+      FORMATTER_PARAMS = {
+        "format" => "@type",
+
+        # TODO: convert to use 'inject'
+        "include_time_key" => "include_time_key",
+        "include_tag_key"  => "include_tag_key",
+        "time_key"      => "time_key",
+        "time_format"   => "time_format",
+        "time_as_epoch" => "time_as_epoch",
+        "localtime" => "localtime",
+        "utc"       => "utc",
+        "timezone"      => "timezone",
+        "tag_key" => "tag_key",
+
+        "delimiter" => "delimiter",
+
+        "force_quotes" => "force_quotes", # CsvFormatter
+        "fields" => "fields", # CsvFormatter
+        "json_parser" => "json_parser", # JSONFormatter
+        "label_delimiter" => "label_delimiter", # LabeledTSVFormatter
+        "output_time" => "output_time", # OutFileFormatter
+        "output_tag"  => "output_tag", # OutFileFormatter
+        "message_key" => "message_key", # SingleValueFormatter
+        "add_newline" => "add_newline", # SingleValueFormatter
+        "output_type" => "output_type", # StdoutFormatter
+      }
+
       def compat_parameters_buffer_default_chunk_key
         # '', 'time' or 'tag'
         raise NotImplementedError, "return one of '', 'time' or 'tag'"
@@ -54,31 +93,43 @@ module Fluent
         case self
         when Fluent::Plugin::Output
           compat_parameters_buffer(conf)
+        when Fluent::Plugin::Parser
+          compat_parameters_parser(conf)
+        when Fluent::Plugin::Formatter
+          compat_parameters_formatter(conf)
         end
 
         super
       end
 
-      def compat_parameters_buffer(conf)
-        # return immediately if <buffer> section exists, or any buffer-related parameters don't exist
-        return unless conf.elements('buffer').empty?
-        return unless (BUFFER_PARAMS_MAP.keys + BUFFER_TIME_SLICED_PARAMS.keys).all?{|k| !conf.has_key?(k) }
-
-        # TODO: warn obsolete parameters if these are deprecated
+      def compat_parameters_copy_to_subsection_attributes(conf, params, &block)
         attr = {}
-        BUFFER_PARAMS_MAP.each do |compat, current|
+        params.each do |compat, current|
           next unless current
           if conf.has_key?(compat)
-            if compat == 'buffer_queue_full_action' && conf[compat] == 'exception'
-              attr[current] = 'throw_exception'
+            if block_given?
+              attr[current] = block.call(compat, conf[compat])
             else
               attr[current] = conf[compat]
             end
           end
         end
-        BUFFER_TIME_SLICED_PARAMS.each do |compat, current|
-          next unless current
-          attr[current] = conf[compat] if conf.has_key?(compat)
+        attr
+      end
+
+      def compat_parameters_buffer(conf)
+        # return immediately if <buffer> section exists, or any buffer-related parameters don't exist
+        return unless conf.elements('buffer').empty?
+        return if (BUFFER_PARAMS.keys + BUFFER_TIME_SLICED_PARAMS.keys).all?{|k| !conf.has_key?(k) }
+
+        # TODO: warn obsolete parameters if these are deprecated
+        buffer_params = BUFFER_PARAMS.merge(BUFFER_TIME_SLICED_PARAMS)
+        attr = compat_parameters_copy_to_subsection_attributes(conf, buffer_params) do |compat_key, value|
+          if compat_key == 'buffer_queue_full_action' && value == 'exception'
+            'throw_exception'
+          else
+            value
+          end
         end
 
         chunk_key = nil
@@ -101,6 +152,32 @@ module Fluent
         end
 
         e = Fluent::Config::Element.new('buffer', chunk_key, attr, [])
+        conf.elements << e
+
+        conf
+      end
+
+      def compat_parameters_parser(conf)
+        return unless conf.elements('parse').empty?
+        return if PARSER_PARAMS.keys.all?{|k| !conf.has_key?(k) }
+
+        # TODO: warn obsolete parameters if these are deprecated
+        attr = compat_parameters_copy_to_subsection_attributes(conf, PARSER_PARAMS)
+
+        e = Fluent::Config::Element.new('parse', '', attr, [])
+        conf.elements << e
+
+        conf
+      end
+
+      def compat_parameters_formatter(conf)
+        return unless conf.elements('format').empty?
+        return if FORMATTER_PARAMS.keys.all?{|k| !conf.has_key?(k) }
+
+        # TODO: warn obsolete parameters if these are deprecated
+        attr = compat_parameters_copy_to_subsection_attributes(conf, FORMATTER_PARAMS)
+
+        e = Fluent::Config::Element.new('format', '', attr, [])
         conf.elements << e
 
         conf
