bcd67efbc789f33c1b1c1ede91257781acb1a372
TAGOMORI Satoshi <tagomoris@gmail.com>
2016-09-02 18:28:55 +0900

    fix tests for new path and namespace

--- test/test_time_parser.rb
+++ test/test_time_parser.rb
@@ -1,6 +1,6 @@
-require_relative '../helper'
-require 'fluent/test/driver/parser'
-require 'fluent/plugin/parser'
+require_relative 'helper'
+require 'fluent/test'
+require 'fluent/time'
 
 class TimeParserTest < ::Test::Unit::TestCase
   def setup
@@ -8,38 +8,38 @@ class TimeParserTest < ::Test::Unit::TestCase
   end
 
   def test_call_with_parse
-    parser = Fluent::Test::Driver::Parser.new(Fluent::Plugin::Parser::TimeParser.new(nil))
+    parser = Fluent::TimeParser.new
 
-    assert(parser.instance.parse('2013-09-18 12:00:00 +0900').is_a?(Fluent::EventTime))
+    assert(parser.parse('2013-09-18 12:00:00 +0900').is_a?(Fluent::EventTime))
 
     time = event_time('2013-09-18 12:00:00 +0900')
-    assert_equal(time, parser.instance.parse('2013-09-18 12:00:00 +0900'))
+    assert_equal(time, parser.parse('2013-09-18 12:00:00 +0900'))
   end
 
   def test_parse_with_strptime
-    parser = Fluent::Test::Driver::Parser.new(Fluent::Plugin::Parser::TimeParser.new('%d/%b/%Y:%H:%M:%S %z'))
+    parser = Fluent::TimeParser.new('%d/%b/%Y:%H:%M:%S %z')
 
-    assert(parser.instance.parse('28/Feb/2013:12:00:00 +0900').is_a?(Fluent::EventTime))
+    assert(parser.parse('28/Feb/2013:12:00:00 +0900').is_a?(Fluent::EventTime))
 
     time = event_time('28/Feb/2013:12:00:00 +0900', format: '%d/%b/%Y:%H:%M:%S %z')
-    assert_equal(time, parser.instance.parse('28/Feb/2013:12:00:00 +0900'))
+    assert_equal(time, parser.parse('28/Feb/2013:12:00:00 +0900'))
   end
 
   def test_parse_nsec_with_strptime
-    parser = Fluent::Test::Driver::Parser.new(Fluent::Plugin::Parser::TimeParser.new('%d/%b/%Y:%H:%M:%S:%N %z'))
+    parser = Fluent::TimeParser.new('%d/%b/%Y:%H:%M:%S:%N %z')
 
-    assert(parser.instance.parse('28/Feb/2013:12:00:00:123456789 +0900').is_a?(Fluent::EventTime))
+    assert(parser.parse('28/Feb/2013:12:00:00:123456789 +0900').is_a?(Fluent::EventTime))
 
     time = event_time('28/Feb/2013:12:00:00:123456789 +0900', format: '%d/%b/%Y:%H:%M:%S:%N %z')
-    assert_equal_event_time(time, parser.instance.parse('28/Feb/2013:12:00:00:123456789 +0900'))
+    assert_equal_event_time(time, parser.parse('28/Feb/2013:12:00:00:123456789 +0900'))
   end
 
   def test_parse_with_invalid_argument
-    parser = Fluent::Test::Driver::Parser.new(Fluent::Plugin::Parser::TimeParser.new(nil))
+    parser = Fluent::TimeParser.new
 
     [[], {}, nil, true, 10000, //, ->{}, '', :symbol].each { |v|
-      assert_raise Fluent::ParserError do
-        parser.instance.parse(v)
+      assert_raise Fluent::TimeParser::TimeParseError do
+        parser.parse(v)
       end
     }
   end
