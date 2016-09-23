d08c38d472eed4a7af3f82c8c1cafc1e82e58e00
Masahiro Nakagawa <repeatedly@gmail.com>
2016-09-23 20:54:29 +0900

    Add tests for formatted and parsed result comparison

--- test/plugin/test_formatter_json.rb
+++ test/plugin/test_formatter_json.rb
@@ -1,4 +1,5 @@
 require_relative '../helper'
+require 'json'
 require 'fluent/test/driver/formatter'
 require 'fluent/plugin/formatter_json'
 
@@ -20,11 +21,23 @@ class JsonFormatterTest < ::Test::Unit::TestCase
     {'message' => 'awesome'}
   end
 
+  def symbolic_record
+    {:message => :awesome}
+  end
+
   data('oj' => 'oj', 'yajl' => 'yajl')
   def test_format(data)
     d = create_driver('json_parser' => data)
     formatted = d.instance.format(tag, @time, record)
 
-    assert_equal("#{Yajl.dump(record)}\n", formatted)
+    assert_equal("#{JSON.generate(record)}\n", formatted)
+  end
+
+  data('oj' => 'oj', 'yajl' => 'yajl')
+  def test_format_with_symbolic_record(data)
+    d = create_driver('json_parser' => data)
+    formatted = d.instance.format(tag, @time, symbolic_record)
+
+    assert_equal("#{JSON.generate(record)}\n", formatted)
   end
 end
