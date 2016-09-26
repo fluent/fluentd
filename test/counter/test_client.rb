8d2192a5cab706985d0d4fea81dce545b6e92c2b
ganmacs <ganmacs@gmail.com>
2016-09-26 11:41:10 +0900

    Add Counter API test

---
+++ test/counter/test_client.rb
@@ -0,0 +1,477 @@
+require_relative '../helper'
+require 'fluent/counter/client'
+require 'fluent/counter/server'
+require 'timecop'
+
+class CounterClientTest < ::Test::Unit::TestCase
+  TEST_HOST = '127.0.0.1'
+  TEST_PORT = '8277'
+
+  setup do
+    # timecop isn't compatible with EventTime
+    t = Time.parse('2016-09-22 16:59:59 +0900')
+    Timecop.freeze(t)
+    @now = Fluent::EventTime.now
+
+    @options = {
+      host: TEST_HOST,
+      port: TEST_PORT,
+    }
+
+    @scope = "server\tplugins"
+    @loop = Coolio::Loop.new
+    @server = Fluent::Counter::Server.new(@options).start
+    @client = Fluent::Counter::Client.new(@loop, @options).start
+  end
+
+  teardown do
+    Timecop.return
+    @server.stop
+    @client.stop
+  end
+
+  test 'Callable API' do
+    [:establish, :init, :delete, :inc, :reset, :get].each do |m|
+      assert_true @client.respond_to?(m)
+    end
+  end
+
+  def extract_value_from_server(server, scope, name)
+    counter = server.instance_variable_get(:@counter)
+    store = counter.instance_variable_get(:@store).instance_variable_get(:@store)
+    key = Fluent::Counter::Store.gen_key(scope, name)
+    store[key]
+  end
+
+  sub_test_case 'establish' do
+    test 'establish a scope' do
+      @client.establish(@scope)
+      assert_equal "somthing_name\t#{@scope}", @client.instance_variable_get(:@scope)
+    end
+
+    data(
+      empty: '',
+      invalid_string: '_scope',
+      invalid_string2: 'Scope'
+    )
+    test 'raise an error when passed scope is invalid' do |scope|
+      assert_raise do
+        @client.establish(scope)
+      end
+    end
+  end
+
+  sub_test_case 'init' do
+    setup do
+      @client.instance_variable_set(:@scope, @scope)
+    end
+
+    data(
+      numeric_type: [
+        { 'name' => 'key', 'reset_interval' => 20, 'type' => 'numeric' }, 0
+      ],
+      float_type: [
+        { 'name' => 'key', 'reset_interval' => 20, 'type' => 'float' }, 0.0
+      ],
+      integer_type: [
+        { 'name' => 'key', 'reset_interval' => 20, 'type' => 'integer' }, 0
+      ]
+    )
+    test 'create a value' do |(param, initial_value)|
+      assert_nil extract_value_from_server(@server, @scope, 'key')
+
+      response = @client.init(param)
+      data = response['data'].first
+
+      assert_nil response['errors']
+      assert_equal 'key', data['name']
+      assert_equal 20, data['reset_interval']
+      assert_equal param['type'], data['type']
+      assert_equal initial_value, data['current']
+      assert_equal initial_value, data['total']
+
+      v = extract_value_from_server(@server, @scope, 'key')
+      assert_equal 'key', v.name
+      assert_equal 20, v.reset_interval
+      assert_equal param['type'], v.type
+      assert_equal initial_value, v.total
+      assert_equal initial_value, v.current
+    end
+
+    data(
+      already_exist_key: [
+        { 'name' => 'key1', 'reset_interval' => 10 },
+        { 'code' => 'invalid_params', 'message' => 'key1 already exists in counter' }
+      ],
+      missing_name: [
+        { 'reset_interval' => 10 },
+        { 'code' => 'invalid_params', 'message' => '`name` is required' },
+      ],
+      missing_reset_interval: [
+        { 'name' => 'key' },
+        { 'code' => 'invalid_params', 'message' => '`reset_interval` is required' },
+      ],
+      invalid_key: [
+        { 'name' => '\tkey' },
+        { 'code' => 'invalid_params', 'message' => '`name` is the invalid format' }
+      ]
+    )
+    test 'return an error object' do |(param, expected_error)|
+      @client.init({ 'name' => 'key1', 'reset_interval' => 10 })
+      resopnse = @client.init(param)
+
+      errors = resopnse['errors'].first
+
+      assert_empty resopnse['data']
+      assert_equal expected_error, errors
+    end
+
+    test 'return an error object and data object' do
+      param = { 'name' => 'key1', 'reset_interval' => 10 }
+      param2 = { 'name' => 'key2', 'reset_interval' => 10 }
+      @client.init(param)
+
+      response = @client.init(param2, param)
+      data = response['data'].first
+      error = response['errors'].first
+
+      assert_equal param2['name'], data['name']
+      assert_equal param2['reset_interval'], data['reset_interval']
+
+      assert_equal 'invalid_params', error['code']
+      assert_equal "#{param['name']} already exists in counter", error['message']
+    end
+
+    test 'return a future object when asyn option is true' do
+      param = { 'name' => 'key', 'reset_interval' => 10 }
+      r = @client.init(param, options: { async: true })
+      assert_true r.is_a?(Fluent::Counter::Future)
+    end
+  end
+
+  sub_test_case 'delete' do
+    setup do
+      @client.instance_variable_set(:@scope, @scope)
+      @name = 'key'
+
+      @init_obj = { 'name' => @name, 'reset_interval' => 20, 'type' => 'numeric' }
+      @client.init(@init_obj)
+    end
+
+    test 'delete a value' do
+      assert extract_value_from_server(@server, @scope, @name)
+
+      response = @client.delete(@name)
+      v = response['data'].first
+
+      assert_nil response['errors']
+      assert_equal @name, v['name']
+      assert_equal 20, v['reset_interval']
+      assert_equal 'numeric', v['type']
+
+      assert_nil extract_value_from_server(@server, @scope, @name)
+    end
+
+    data(
+      key_not_found: [
+        'key2',
+        { 'code' => 'unknown_key', 'message' => "`key2` doesn't exist in counter" }
+      ],
+      invalid_key: [
+        '\tkey',
+        { 'code' => 'invalid_params', 'message' => '`key` is the invalid format' }
+      ]
+    )
+    test 'return an error object' do |(param, expected_error)|
+      resopnse = @client.delete(param)
+
+      errors = resopnse['errors'].first
+
+      assert_empty resopnse['data']
+      assert_equal expected_error, errors
+    end
+
+    test 'return an error object and data object' do
+      unknow_name = 'key2'
+
+      response = @client.delete(@name, unknow_name)
+      data = response['data'].first
+      error = response['errors'].first
+
+      assert_equal @name, data['name']
+      assert_equal @init_obj['reset_interval'], data['reset_interval']
+
+      assert_equal 'unknown_key', error['code']
+      assert_equal "`key2` doesn't exist in counter", error['message']
+
+      assert_nil extract_value_from_server(@server, @scope, @name)
+    end
+
+    test 'return a future object when asyn option is true' do
+      r = @client.delete(@name, options: { async: true })
+      assert_true r.is_a?(Fluent::Counter::Future)
+    end
+  end
+
+  sub_test_case 'inc' do
+    setup do
+      @client.instance_variable_set(:@scope, @scope)
+      @name = 'key'
+
+      @init_obj = { 'name' => @name, 'reset_interval' => 20, 'type' => 'numeric' }
+      @client.init(@init_obj)
+    end
+
+    test 'increment a value' do
+      v = extract_value_from_server(@server, @scope, @name)
+      assert_equal 0, v.total
+      assert_equal 0, v.current
+
+      @client.inc({ 'name' => @name, 'value' => 10 })
+
+      v = extract_value_from_server(@server, @scope, @name)
+      assert_equal 10, v.total
+      assert_equal 10, v.current
+      assert_equal @now, v.last_modified_at
+    end
+
+    test 'create and increment a value when force option is true' do
+      name = 'new_key'
+      param = { 'name' => name, 'value' => 11, 'reset_interval' => 1 }
+
+      assert_nil extract_value_from_server(@server, @scope, name)
+
+      @client.inc(param, options: { force: true })
+
+      v = extract_value_from_server(@server, @scope, name)
+      assert_equal name, v.name
+      assert_equal 1, v.reset_interval
+      assert_equal 11, v.total
+      assert_equal 11, v.total
+    end
+
+    data(
+      not_exist_key: [
+        { 'name' => 'key2', 'value' => 10 },
+        { 'code' => 'unknown_key', 'message' => "`key2` doesn't exist in counter" }
+      ],
+      missing_name: [
+        { 'value' => 10 },
+        { 'code' => 'invalid_params', 'message' => '`name` is required' },
+      ],
+      missing_value: [
+        { 'name' => 'key' },
+        { 'code' => 'invalid_params', 'message' => '`value` is required' },
+      ],
+      invalid_key: [
+        { 'name' => '\tkey' },
+        { 'code' => 'invalid_params', 'message' => '`name` is the invalid format' }
+      ]
+    )
+    test 'return an error object' do |(param, expected_error)|
+      resopnse = @client.inc(param)
+
+      errors = resopnse['errors'].first
+      assert_empty resopnse['data']
+      assert_equal expected_error, errors
+    end
+
+    test 'return an error object and data object' do
+      parmas = [
+        { 'name' => @name, 'value' => 10 },
+        { 'name' => 'unknown_key', 'value' => 9 }
+      ]
+      response = @client.inc(*parmas)
+
+      data = response['data'].first
+      error = response['errors'].first
+
+      assert_equal @name, data['name']
+      assert_equal 10, data['current']
+      assert_equal 10, data['total']
+
+      assert_equal 'unknown_key', error['code']
+      assert_equal "`unknown_key` doesn't exist in counter", error['message']
+    end
+
+    test 'return a future object when asyn option is true' do
+      param = { 'name' => 'key', 'value' => 10 }
+      r = @client.inc(param, options: { async: true })
+      assert_true r.is_a?(Fluent::Counter::Future)
+    end
+  end
+
+  sub_test_case 'get' do
+    setup do
+      @client.instance_variable_set(:@scope, @scope)
+      @name = 'key'
+
+      @init_obj = { 'name' => @name, 'reset_interval' => 20, 'type' => 'numeric' }
+      @client.init(@init_obj)
+    end
+
+    test 'get a value' do
+      v1 = extract_value_from_server(@server, @scope, @name)
+      v2 = @client.get(@name)['data'].first
+
+      assert_equal v1.name, v2['name']
+      assert_equal v1.current, v2['current']
+      assert_equal v1.total, v2['total']
+      assert_equal v1.type, v2['type']
+    end
+
+    data(
+      key_not_found: [
+        'key2',
+        { 'code' => 'unknown_key', 'message' => "`key2` doesn't exist in counter" }
+      ],
+      invalid_key: [
+        '\tkey',
+        { 'code' => 'invalid_params', 'message' => '`key` is the invalid format' }
+      ]
+    )
+    test 'return an error object' do |(param, expected_error)|
+      resopnse = @client.get(param)
+
+      errors = resopnse['errors'].first
+
+      assert_empty resopnse['data']
+      assert_equal expected_error, errors
+    end
+
+    test 'return an error object and data object' do
+      unknow_name = 'key2'
+
+      response = @client.get(@name, unknow_name)
+      data = response['data'].first
+      error = response['errors'].first
+
+      assert_equal @name, data['name']
+      assert_equal @init_obj['reset_interval'], data['reset_interval']
+
+      assert_equal 'unknown_key', error['code']
+      assert_equal "`key2` doesn't exist in counter", error['message']
+    end
+
+    test 'return a future object when asyn option is true' do
+      r = @client.get(@name, options: { async: true })
+      assert_true r.is_a?(Fluent::Counter::Future)
+    end
+  end
+
+  sub_test_case 'rseet' do
+    setup do
+      @client.instance_variable_set(:@scope, @scope)
+      @name = 'key'
+
+      @init_obj = { 'name' => @name, 'reset_interval' => 5, 'type' => 'numeric' }
+      @client.init(@init_obj)
+      @client.inc({ 'name' => @name, 'value' => 10 })
+    end
+
+    test 'reset a value after `reset_interval` passed' do
+      v1 = extract_value_from_server(@server, @scope, @name)
+      assert_equal 10, v1.total
+      assert_equal 10, v1.current
+      assert_equal @now, v1.last_reset_at
+
+      Timecop.travel(6)         # greater than reset_interval
+
+      v2 = @client.reset(@name)
+      data = v2['data'].first
+
+      c = data['counter_data']
+
+      assert_equal 6, data['elapsed_time']
+      assert_true data['success']
+
+      assert_equal 10, c['current']
+      assert_equal 10, c['total']
+      assert_equal @now, c['last_reset_at']
+
+      v1 = extract_value_from_server(@server, @scope, @name)
+      assert_equal 0, v1.current
+      assert_equal 10, v1.total
+      assert_equal (@now + 6), v1.last_reset_at
+      assert_equal (@now + 6), v1.last_modified_at
+    end
+
+    test 'return a value object before `reset_interval` passed' do
+      v1 = extract_value_from_server(@server, @scope, @name)
+      assert_equal 10, v1.total
+      assert_equal 10, v1.current
+      assert_equal @now, v1.last_reset_at
+
+      Timecop.travel(4)         # less than reset_interval
+
+      v2 = @client.reset(@name)
+      data = v2['data'].first
+
+      c = data['counter_data']
+
+      assert_equal 4, data['elapsed_time']
+      assert_equal false, data['success']
+
+      assert_equal 10, c['current']
+      assert_equal 10, c['total']
+      assert_equal @now, c['last_reset_at']
+
+      v1 = extract_value_from_server(@server, @scope, @name)
+      assert_equal 10, v1.current
+      assert_equal 10, v1.total
+      assert_equal @now, v1.last_reset_at
+      assert_equal @now, v1.last_reset_at
+    end
+
+    data(
+      key_not_found: [
+        'key2',
+        { 'code' => 'unknown_key', 'message' => "`key2` doesn't exist in counter" }
+      ],
+      invalid_key: [
+        '\tkey',
+        { 'code' => 'invalid_params', 'message' => '`key` is the invalid format' }
+      ]
+    )
+    test 'return an error object' do |(param, expected_error)|
+      resopnse = @client.reset(param)
+
+      errors = resopnse['errors'].first
+
+      assert_empty resopnse['data']
+      assert_equal expected_error, errors
+    end
+
+    test 'return an error object and data object' do
+      unknow_name = 'key2'
+
+      Timecop.travel(6)         # greater than reset_interval
+
+      response = @client.reset(@name, unknow_name)
+      data = response['data'].first
+      error = response['errors'].first
+      counter = data['counter_data']
+
+      assert_true data['success']
+      assert_equal 6, data['elapsed_time']
+      assert_equal @name, counter['name']
+      assert_equal @init_obj['reset_interval'], counter['reset_interval']
+      assert_equal 10, counter['total']
+      assert_equal 10, counter['current']
+
+      assert_equal 'unknown_key', error['code']
+      assert_equal "`key2` doesn't exist in counter", error['message']
+
+      v1 = extract_value_from_server(@server, @scope, @name)
+      assert_equal 0, v1.current
+      assert_equal 10, v1.total
+      assert_equal (@now + 6), v1.last_reset_at
+      assert_equal (@now + 6), v1.last_modified_at
+    end
+
+    test 'return a future object when asyn option is true' do
+      r = @client.reset(@name, options: { async: true })
+      assert_true r.is_a?(Fluent::Counter::Future)
+    end
+  end
+end
---
+++ test/counter/test_store.rb
@@ -0,0 +1,345 @@
+require_relative '../helper'
+require 'fluent/counter/store'
+require 'timecop'
+
+class CounterStoreValueTest < ::Test::Unit::TestCase
+  setup do
+    # timecop isn't compatible with EventTime
+    t = Time.parse('2016-09-22 16:59:59 +0900')
+    Timecop.freeze(t)
+    @now = Fluent::EventTime.now
+  end
+
+  teardown do
+    Timecop.return
+  end
+
+  sub_test_case 'init' do
+    test 'default values' do
+      v = Fluent::Counter::Store::Value.init(
+        'name' => 'name',
+        'reset_interval' => 10
+      )
+      assert_equal 'name', v.name
+      assert_equal 10, v.reset_interval
+      assert_equal 'numeric', v.type
+      assert_equal @now, v.last_reset_at
+      assert_equal @now, v.last_modified_at
+      assert_equal 0, v.current
+      assert_equal 0, v.total
+    end
+
+    data(
+      integer: ['integer', 0, Integer],
+      numeric: ['numeric', 0, Numeric],
+      flaot: ['float', 0.0, Float]
+    )
+    test 'set a type' do |(type_name, value, type)|
+      v = Fluent::Counter::Store::Value.init(
+        'type' => type_name,
+        'name' => 'name',
+        'reset_interval' => 10
+      )
+
+      assert_true v.current.is_a?(type)
+      assert_equal value, v.current
+      assert_equal value, v.total
+      assert_equal type_name, v.type
+    end
+
+    test 'raise an error when an invalid type has passed' do
+      assert_raise do
+        Fluent::Counter::Store::Value.init(
+          'type' => 'invalid_type',
+          'name' => 'name',
+          'reset_interval' => 10
+        )
+      end
+    end
+  end
+
+  sub_test_case 'initial_value' do
+    data(
+      integer: ['integer', Integer],
+      numeric: ['numeric', Integer],
+      float: ['float', Float],
+    )
+    test 'return a initial value' do |(type, type_class)|
+      assert_true Fluent::Counter::Store::Value.initial_value(type).is_a?(type_class)
+    end
+
+    test 'raise an error when passed string is invalid' do
+      assert_raise Fluent::Counter::InvalidParams do
+        Fluent::Counter::Store::Value.initial_value('invalid value')
+      end
+    end
+  end
+
+  test 'to_response_hash' do
+    v = Fluent::Counter::Store::Value.init(
+      'name' => 'name',
+      'reset_interval' => 10
+    )
+    expected = {
+      'name' => 'name',
+      'total' => 0,
+      'current' => 0,
+      'type' => 'numeric',
+      'reset_interval' => 10,
+      'last_reset_at' => @now,
+    }
+    assert_equal expected, v.to_response_hash
+  end
+end
+
+class CounterStoreTest < ::Test::Unit::TestCase
+  setup do
+    @name = 'key_name'
+    @scope = "server\tworker\tplugin"
+
+    # timecop isn't compatible with EventTime
+    t = Time.parse('2016-09-22 16:59:59 +0900')
+    Timecop.freeze(t)
+    @now = Fluent::EventTime.now
+  end
+
+  shutdown do
+    Timecop.return
+  end
+
+  def extract_value_from_counter(counter, scope, name)
+    store = counter.instance_variable_get(:@store)
+    key = Fluent::Counter::Store.gen_key(scope, name)
+    store[key]
+  end
+
+  sub_test_case 'init' do
+    setup do
+      @reset_interval = 10
+      @store = Fluent::Counter::Store.new
+      @data = { 'name' => @name, 'reset_interval' => @reset_interval }
+    end
+
+    test 'create new value in the counter' do
+      v = @store.init(@name, @scope, @data)
+
+      assert_equal @name, v.name
+      assert_equal @reset_interval, v.reset_interval
+
+      v2 = extract_value_from_counter(@store, @scope, @name)
+      assert_equal v, v2
+    end
+
+    test 'raise an error when a passed key already exists' do
+      @store.init(@name, @scope, @data)
+
+      assert_raise Fluent::Counter::InvalidParams do
+        @store.init(@name, @scope, @data)
+      end
+    end
+
+    test 'return a value when passed key already exists and a ignore option is true' do
+      v = @store.init(@name, @scope, @data)
+      v1 = extract_value_from_counter(@store, @scope, @name)
+      v2 = @store.init(@name, @scope, @data, ignore: true)
+      assert_equal v, v2
+      assert_equal v1, v2
+    end
+  end
+
+  sub_test_case 'get' do
+    setup do
+      @store = Fluent::Counter::Store.new
+      data = { 'name' => @name, 'reset_interval' => 10 }
+      @store.init(@name, @scope, data)
+    end
+
+    test 'return a value from the counter' do
+      v = extract_value_from_counter(@store, @scope, @name)
+      assert_equal v, @store.get(@name, @scope)
+    end
+
+    test "return nil when a passed key doesn't exist" do
+      assert_equal nil, @store.get('unknown_key', @scope)
+      assert_equal nil, @store.get(@name, 'unknown_scope')
+    end
+
+    test "raise a error when when a passed key doesn't exist and raise_error option is true" do
+      assert_raise Fluent::Counter::UnknownKey do
+        @store.get('unkonwn_key', @scope, raise_error: true)
+      end
+
+      assert_raise Fluent::Counter::UnknownKey do
+        @store.get(@name, 'unknown_key', raise_error: true)
+      end
+    end
+  end
+
+  sub_test_case 'key?' do
+    setup do
+      @store = Fluent::Counter::Store.new
+      data = { 'name' => @name, 'reset_interval' => 10 }
+      @store.init(@name, @scope, data)
+    end
+
+    test 'return true when passed key exists' do
+      assert_true @store.key?(@name, @scope)
+    end
+
+    test "return false when passed key doesn't exist" do
+      assert_true !@store.key?('unknown_key', @scope)
+      assert_true !@store.key?(@name, 'unkonwn_scope')
+    end
+  end
+
+  sub_test_case 'delete' do
+    setup do
+      @store = Fluent::Counter::Store.new
+      data = { 'name' => @name, 'reset_interval' => 10 }
+      @init_value = @store.init(@name, @scope, data)
+    end
+
+    test 'delete a value from the counter' do
+      v = @store.delete(@name, @scope)
+      assert_equal @init_value, v
+      assert_nil extract_value_from_counter(@store, @scope, @name)
+    end
+
+    test "raise an error when passed key doesn't exist" do
+      assert_raise Fluent::Counter::UnknownKey do
+        @store.delete('unknown_key', @scope)
+      end
+
+      assert_raise Fluent::Counter::UnknownKey do
+        @store.delete(@name, 'unknown_key')
+      end
+    end
+  end
+
+  sub_test_case 'inc' do
+    setup do
+      @store = Fluent::Counter::Store.new
+      @init_data = { 'name' => @name, 'reset_interval' => 10 }
+      @travel_sec = 10
+    end
+
+    data(
+      positive: 10,
+      negative: -10
+    )
+    test 'increment or decrement a value in the counter' do |value|
+      @store.init(@name, @scope, @init_data)
+      Timecop.travel(@travel_sec)
+      v = @store.inc(@name, @scope, { 'value' => value })
+
+      assert_equal value, v.total
+      assert_equal value, v.current
+      assert_equal (@now + @travel_sec), v.last_modified_at
+      assert_equal @now, v.last_reset_at # last_reset_at doesn't change
+
+      v1 = extract_value_from_counter(@store, @scope, @name)
+      assert_equal v, v1
+    end
+
+    test "raise an error when passed key doesn't exist" do
+      assert_raise Fluent::Counter::UnknownKey do
+        @store.inc('unknown_key', @scope, { 'value' => 1 })
+      end
+
+      assert_raise Fluent::Counter::UnknownKey do
+        @store.inc(@name, 'unknown_key', { 'value' => 1 })
+      end
+    end
+
+    test 'raise an error when a tyep of passed value is incompatible with a stored value' do
+      name2 = 'key_name2'
+      name3 = 'key_name3'
+      v = @store.init(@name, @scope, @init_data.merge('type' => 'integer'))
+      v2 = @store.init(name2, @scope, @init_data.merge('type' => 'float'))
+      v3 = @store.init(name3, @scope, @init_data.merge('type' => 'numeric'))
+      assert_equal 'integer', v.type
+      assert_equal 'float', v2.type
+      assert_equal 'numeric', v3.type
+
+      assert_raise Fluent::Counter::InvalidParams do
+        @store.inc(@name, @scope, { 'value' => 1.1 })
+      end
+
+      assert_raise Fluent::Counter::InvalidParams do
+        @store.inc(name2, @scope, { 'value' => 1 })
+      end
+
+      assert_nothing_raised do
+        @store.inc(name3, @scope, { 'value' => 1 })
+        @store.inc(name3, @scope, { 'value' => 1.0 })
+      end
+    end
+  end
+
+  sub_test_case 'reset' do
+    setup do
+      @store = Fluent::Counter::Store.new
+      @travel_sec = 10
+
+      @inc_value = 10
+      @store.init(@name, @scope, { 'name' => @name, 'reset_interval' => 10 })
+      @store.inc(@name, @scope, { 'value' => 10 })
+    end
+
+    test 'reset a value in the counter' do
+      Timecop.travel(@travel_sec)
+
+      v = @store.reset(@name, @scope)
+      assert_equal @travel_sec, v['elapsed_time']
+      assert_true v['success']
+      counter = v['counter_data']
+
+      assert_equal @name, counter['name']
+      assert_equal @inc_value, counter['total']
+      assert_equal @inc_value, counter['current']
+      assert_equal 'numeric', counter['type']
+      assert_equal @now, counter['last_reset_at']
+      assert_equal 10, counter['reset_interval']
+
+      v1 = extract_value_from_counter(@store, @scope, @name)
+      assert_equal 0, v1.current
+      assert_true v1.current.is_a?(Integer)
+      assert_equal @inc_value, v1.total
+      assert_equal (@now + @travel_sec), v1.last_reset_at
+      assert_equal (@now + @travel_sec), v1.last_modified_at
+    end
+
+    test 'reset a value after `reset_interval` passed' do
+      first_travel_sec = 5
+      Timecop.travel(first_travel_sec) # jump time less than reset_interval
+      v = @store.reset(@name, @scope)
+
+      assert_equal false, v['success']
+      assert_equal first_travel_sec, v['elapsed_time']
+      store = extract_value_from_counter(@store, @scope, @name)
+      assert_equal 10, store.current
+      assert_equal @now, store.last_reset_at
+
+      # time is passed greater than reset_interval
+      Timecop.travel(@travel_sec)
+      v = @store.reset(@name, @scope)
+      assert_true v['success']
+      assert_equal @travel_sec + first_travel_sec, v['elapsed_time']
+
+      v1 = extract_value_from_counter(@store, @scope, @name)
+      assert_equal 0, v1.current
+      assert_equal (@now + @travel_sec + first_travel_sec), v1.last_reset_at
+      assert_equal (@now + @travel_sec + first_travel_sec), v1.last_modified_at
+    end
+
+    test "raise an error when passed key doesn't exist" do
+      assert_raise Fluent::Counter::UnknownKey do
+        @store.reset('unknown_key', @scope)
+      end
+
+      assert_raise Fluent::Counter::UnknownKey do
+        @store.reset(@name, 'unknown_key')
+      end
+    end
+  end
+end
