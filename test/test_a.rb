4ee8f881ec147272b75cf29648f9cb7cba4b208e
NARUSE, Yui <naruse@airemix.jp>
2016-11-22 20:07:33 +0900

    wip

--- test/test_a.rb
+++ test/test_a.rb
@@ -1,17 +1,39 @@
 #!/usr/bin/env ruby
+require 'serverengine'
 $: << File.expand_path('../../lib', __FILE__)
-require_relative 'helper'
+require 'fluent/plugin_id'
+require 'fluent/log'
+require 'fluent/env'
+require 'fluent/engine'
+require 'fluent/plugin_helper'
 require 'fluent/plugin_helper/server'
 require 'fluent/plugin/base'
 
-require 'serverengine'
 
 class Dummy < Fluent::Plugin::Base
-  include PluginId
-  include PluginLoggerMixin
-  include PluginHelper::Mixin
+  include Fluent::PluginId
+  include Fluent::PluginLoggerMixin
+  include Fluent::PluginHelper::Mixin
   helpers :server
 end
+
+def unused_port(num = 1)
+  ports = []
+  sockets = []
+  num.times do
+    s = TCPServer.open(0)
+    sockets << s
+    ports << s.addr[1]
+  end
+  sockets.each{|s| s.close }
+  if num == 1
+    return ports.first
+  else
+    return *ports
+  end
+end
+logger = ServerEngine::DaemonLogger.new(STDERR)
+$log ||= Fluent::Log.new(logger)
 
 PORT = unused_port
 
@@ -29,6 +51,9 @@ begin
   d2 = Dummy.new; d2.start; d2.after_start
   d.server_create(:myserver, PORT, proto: :tcp, shared: false){|x| x }
   STDERR.puts "#{__LINE__}: #{d2} running..."
+  meth = d2.method(:server_create)
+  p meth
+  p meth.source_location
   d2.server_create(:myserver, PORT, proto: :tcp){|x| x }
   STDERR.puts "#{__LINE__}: running..."
 ensure
