e36f59721d364cdfc0507d6c0f7278fe6ddccf39
NARUSE, Yui <naruse@airemix.jp>
2016-11-22 19:49:58 +0900

    wip

--- test/test_a.rb
+++ test/test_a.rb
@@ -15,7 +15,7 @@ class AToFailAtFirstTest < Test::Unit::TestCase
 
   PORT = unused_port
 
-  setup do
+  def test_foo
     @socket_manager_path = ServerEngine::SocketManager::Server.generate_path
     if @socket_manager_path.is_a?(String) && File.exist?(@socket_manager_path)
       FileUtils.rm_f @socket_manager_path
@@ -26,43 +26,19 @@ class AToFailAtFirstTest < Test::Unit::TestCase
     @d = Dummy.new
     @d.start
     @d.after_start
-  end
-
-  teardown do
-    @d.stopped? || @d.stop
-    @d.before_shutdown? || @d.before_shutdown
-    @d.shutdown? || @d.shutdown
-    @d.after_shutdown? || @d.after_shutdown
-    @d.closed? || @d.close
-    @d.terminated? || @d.terminate
-
-    @socket_manager_server.close
-    if @socket_manager_server.is_a?(String) && File.exist?(@socket_manager_path)
-      FileUtils.rm_f @socket_manager_path
-    end
-  end
-
-    def test_foo
-      m, proto, kwargs = *[:server_create, :tcp, {}]
-      STDERR.puts "#{__LINE__}: running..."
-      begin
-        d2 = Dummy.new; d2.start; d2.after_start
-      STDERR.puts "#{__LINE__}: running..."
-
-      STDERR.puts "#{__LINE__}: running..."
-        assert_nothing_raised do
-      STDERR.puts "#{__LINE__}: running..."
-          @d.__send__(m, :myserver, PORT, proto: proto, shared: false, **kwargs){|x| x }
-      STDERR.puts "#{__LINE__}: running..."
-        end
-      STDERR.puts "#{__LINE__}: #{d2} #{m} #{proto} #{kwargs} running..."
-          d2.server_create(:myserver, PORT, proto: proto, **kwargs){|x| x }
-      STDERR.puts "#{__LINE__}: running..."
-      ensure
-      STDERR.puts "#{__LINE__}: running..."
-        d2.stop; d2.before_shutdown; d2.shutdown; d2.after_shutdown; d2.close; d2.terminate
-      STDERR.puts "#{__LINE__}: running..."
+    m, proto, kwargs = *[:server_create, :tcp, {}]
+    begin
+      d2 = Dummy.new; d2.start; d2.after_start
+      assert_nothing_raised do
+        @d.__send__(m, :myserver, PORT, proto: proto, shared: false, **kwargs){|x| x }
       end
+      STDERR.puts "#{__LINE__}: #{d2} #{m} #{proto} #{kwargs} running..."
+      d2.server_create(:myserver, PORT, proto: proto, **kwargs){|x| x }
       STDERR.puts "#{__LINE__}: running..."
+    ensure
+      STDERR.puts "#{__LINE__}: running..."
+      d2.stop; d2.before_shutdown; d2.shutdown; d2.after_shutdown; d2.close; d2.terminate
+    end
+    STDERR.puts "#{__LINE__}: running..."
   end
 end
