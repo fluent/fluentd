aae760ee4091a72b362025c03478a6a22e2321f5
TAGOMORI Satoshi <tagomoris@gmail.com>
2016-11-24 11:54:13 +0900

    add helper method to get unused UDP ports

--- test/helper.rb
+++ test/helper.rb
@@ -69,13 +69,48 @@ end
 
 include Fluent::Test::Helpers
 
-def unused_port(num = 1)
+def unused_port(num = 1, protocol: :tcp, bind: "0.0.0.0")
+  case protocol
+  when :tcp
+    unused_port_tcp(num)
+  when :udp
+    unused_port_udp(num, bind: bind)
+  else
+    raise ArgumentError, "unknown protocol: #{protocol}"
+  end
+end
+
+def unused_port_tcp(num = 1)
   ports = []
   sockets = []
   num.times do
     s = TCPServer.open(0)
     sockets << s
     ports << s.addr[1]
+  end
+  sockets.each{|s| s.close }
+  if num == 1
+    return ports.first
+  else
+    return *ports
+  end
+end
+
+PORT_RANGE_AVAILABLE = (1024...65535)
+
+def unused_port_udp(num = 1, bind: "0.0.0.0")
+  family = IPAddr.new(IPSocket.getaddress(bind)).ipv4? ? ::Socket::AF_INET : ::Socket::AF_INET6
+  ports = []
+  sockets = []
+  while ports.size < num
+    port = rand(PORT_RANGE_AVAILABLE)
+    u = UDPSocket.new(family)
+    if (u.bind(bind, port) rescue nil)
+      ports << port
+      sockets << u
+    else
+      u.close
+    end
   end
   sockets.each{|s| s.close }
   if num == 1
