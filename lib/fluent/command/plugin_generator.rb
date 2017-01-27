79f146af7599f763a4b13444d1652881031cd86e
Kenji Okimoto <okimoto@clear-code.com>
2017-01-27 17:50:34 +0900

    Download license from web site

--- lib/fluent/command/plugin_generator.rb
+++ lib/fluent/command/plugin_generator.rb
@@ -18,20 +18,21 @@ require "optparse"
 require "pathname"
 require "fileutils"
 require "erb"
+require "open-uri"
 
 class FluentPluginGenerator
   attr_reader :type, :name
-  attr_reader :license
+  attr_reader :license_name
 
   LICENSES_MAP = {
-    "Apache-2.0" => "Apache License, Version 2.0"
+    "Apache-2.0" => ApacheLicense
   }
 
   def initialize(argv = ARGV)
     @argv = argv
     @parser = prepare_parser
 
-    @license = "Apache-2.0"
+    @license_name = "Apache-2.0"
     @overwrite_all = false
   end
 
@@ -42,8 +43,6 @@ class FluentPluginGenerator
       copy_license
       template_directory.find do |path|
         next if path.directory?
-        next if path.fnmatch?("*/preambles/*")
-        next if path.fnmatch?("*/licenses/*")
         dest_dir = path.dirname.sub(/\A#{Regexp.quote(template_directory.to_s)}\/?/, "")
         dest_file = dest_filename(path)
         if path.extname == ".erb"
@@ -97,7 +96,7 @@ Usage: fluent-plugin-generate [options] <type> <name>
 BANNER
 
     @parser.on("--[no-]license=NAME", "Specify license name") do |v|
-      @license = v
+      @license_name = v
     end
     @parser
   end
@@ -173,24 +172,20 @@ BANNER
   end
 
   def preamble
-    return "" unless license
-    preamble_file = template_file("preambles/#{license}.erb")
-    return "" unless preamble_file.exist?
-    src = preamble_file.read
-    ERB.new(src, nil, "-").result(binding).lines.map {|line| "# #{line}".gsub(/ +$/, "") }.join
-  end
-
-  def license_full_name
-    LICENSES_MAP[license]
+    return "" unless license_name
+    return "" unless @license
+    @license.preamble(user_name).lines.map {|line| "# #{line}".gsub(/ +$/, "") }.join
   end
 
   def copy_license
     # in gem_name directory
-    return unless license
-    puts "License: #{license}"
-    license_file = template_file("licenses/#{license}.txt")
-    return unless license_file.exist?
-    file(license_file, Pathname("LICENSE"))
+    return unless license_name
+    puts "License: #{license_name}"
+    @license = LICENSE_MAP[license_name].new
+    unless @license
+      usage("Unknown license: #{license_name}")
+    end
+    Pathname("LICENSE").write(@license.license)
   end
 
   def create_label(dest, contents)
@@ -229,6 +224,30 @@ BANNER
 HELP
       end
       puts "Retrying..."
+    end
+  end
+
+  class ApacheLicense
+    LICENSE_URL = "http://www.apache.org/licenses/LICENSE-2.0.txt"
+
+    attr_reader :license
+
+    def initialize
+      @license = ""
+      @preamble_source = ""
+      @preamble = nil
+      open(LICENSE_URL) do |io|
+        @license = io.read
+      end
+      @preamble_source = @license[/^(\s*Copyright.+)/m, 1]
+    end
+
+    def full_name
+      "Apache License, Version 2.0"
+    end
+
+    def preamble(name)
+      @preamble ||= @preamble_source.gsub(/\[yyyy\]/, "#{Date.today.year}-").gsub(/\[name of copyright owner\]/, name)
     end
   end
 end
