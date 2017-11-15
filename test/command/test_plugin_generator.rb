a75e2a55e4f3c0c0cd0286fa0b8f9a4a31a2bcc1
Kenji Okimoto <okimoto@clear-code.com>
2017-11-15 17:40:19 +0900

    Indent

--- test/command/test_plugin_generator.rb
+++ test/command/test_plugin_generator.rb
@@ -5,64 +5,64 @@ require 'fluent/command/plugin_generator'
 
 class TestFluentPluginGenerator < Test::Unit::TestCase
   sub_test_case "generate plugin" do
-  TEMP_DIR = "tmp/plugin_generator"
-  setup do
-    FileUtils.mkdir_p(TEMP_DIR)
-    @pwd = Dir.pwd
-    Dir.chdir(TEMP_DIR)
-  end
-
-  teardown do
-    Dir.chdir(@pwd)
-    FileUtils.rm_rf(TEMP_DIR)
-  end
-
-  data(input: ["input", "in"],
-       output: ["output", "out"],
-       filter: ["filter", "filter"],
-       parser: ["parser", "parser"],
-       formatter: ["formatter", "formatter"])
-  test "generate plugin" do |(type, part)|
-    capture_stdout do
-      FluentPluginGenerator.new([type, "fake"]).call
+    TEMP_DIR = "tmp/plugin_generator"
+    setup do
+      FileUtils.mkdir_p(TEMP_DIR)
+      @pwd = Dir.pwd
+      Dir.chdir(TEMP_DIR)
     end
-    plugin_base_dir = Pathname("fluent-plugin-fake")
-    assert { plugin_base_dir.directory? }
-    expected = [
-      "fluent-plugin-fake",
-      "fluent-plugin-fake/Gemfile",
-      "fluent-plugin-fake/LICENSE",
-      "fluent-plugin-fake/README.md",
-      "fluent-plugin-fake/Rakefile",
-      "fluent-plugin-fake/fluent-plugin-fake.gemspec",
-      "fluent-plugin-fake/lib",
-      "fluent-plugin-fake/lib/fluent",
-      "fluent-plugin-fake/lib/fluent/plugin",
-      "fluent-plugin-fake/lib/fluent/plugin/#{part}_fake.rb",
-      "fluent-plugin-fake/test",
-      "fluent-plugin-fake/test/helper.rb",
-      "fluent-plugin-fake/test/plugin",
-      "fluent-plugin-fake/test/plugin/test_#{part}_fake.rb",
-    ]
-    actual = plugin_base_dir.find.reject {|f| f.fnmatch("*/.git*") }.map(&:to_s).sort
-    assert_equal(expected, actual)
-  end
 
-  test "no license" do
-    capture_stdout do
-      FluentPluginGenerator.new(["--no-license", "filter", "fake"]).call
+    teardown do
+      Dir.chdir(@pwd)
+      FileUtils.rm_rf(TEMP_DIR)
     end
-    assert { !Pathname("fluent-plugin-fake/LICENSE").exist? }
-    assert { Pathname("fluent-plugin-fake/Gemfile").exist? }
-  end
 
-  test "unknown license" do
-    out = capture_stdout do
-      assert_raise(SystemExit) do
-        FluentPluginGenerator.new(["--license=unknown", "filter", "fake"]).call
+    data(input: ["input", "in"],
+         output: ["output", "out"],
+         filter: ["filter", "filter"],
+         parser: ["parser", "parser"],
+         formatter: ["formatter", "formatter"])
+    test "generate plugin" do |(type, part)|
+      capture_stdout do
+        FluentPluginGenerator.new([type, "fake"]).call
       end
+      plugin_base_dir = Pathname("fluent-plugin-fake")
+      assert { plugin_base_dir.directory? }
+      expected = [
+        "fluent-plugin-fake",
+        "fluent-plugin-fake/Gemfile",
+        "fluent-plugin-fake/LICENSE",
+        "fluent-plugin-fake/README.md",
+        "fluent-plugin-fake/Rakefile",
+        "fluent-plugin-fake/fluent-plugin-fake.gemspec",
+        "fluent-plugin-fake/lib",
+        "fluent-plugin-fake/lib/fluent",
+        "fluent-plugin-fake/lib/fluent/plugin",
+        "fluent-plugin-fake/lib/fluent/plugin/#{part}_fake.rb",
+        "fluent-plugin-fake/test",
+        "fluent-plugin-fake/test/helper.rb",
+        "fluent-plugin-fake/test/plugin",
+        "fluent-plugin-fake/test/plugin/test_#{part}_fake.rb",
+      ]
+      actual = plugin_base_dir.find.reject {|f| f.fnmatch("*/.git*") }.map(&:to_s).sort
+      assert_equal(expected, actual)
     end
-    assert { out.lines.include?("License: unknown\n") }
-  end
+
+    test "no license" do
+      capture_stdout do
+        FluentPluginGenerator.new(["--no-license", "filter", "fake"]).call
+      end
+      assert { !Pathname("fluent-plugin-fake/LICENSE").exist? }
+      assert { Pathname("fluent-plugin-fake/Gemfile").exist? }
+    end
+
+    test "unknown license" do
+      out = capture_stdout do
+        assert_raise(SystemExit) do
+          FluentPluginGenerator.new(["--license=unknown", "filter", "fake"]).call
+        end
+      end
+      assert { out.lines.include?("License: unknown\n") }
+    end
   end
 end
