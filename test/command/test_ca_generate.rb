6cfbc2c9b47935adaceb88b6614e957c2920f2c0
Hiroshi Hatake <hatake@clear-code.com>
2018-01-16 14:10:50 +0900

    Use DEFAULT_OPTIONS constant in test case

--- test/command/test_ca_generate.rb
+++ test/command/test_ca_generate.rb
@@ -9,14 +9,7 @@ require 'fluent/event'
 
 class TestFluentCaGenerate < ::Test::Unit::TestCase
   def test_generate_ca_pair
-    opt = {
-      private_key_length: 2048,
-      cert_country:  'US',
-      cert_state:    'CA',
-      cert_locality: 'Mountain View',
-      cert_common_name: 'Fluentd Forward CA',
-    }
-    cert, key = Fluent::CaGenerate.generate_ca_pair(opt)
+    cert, key = Fluent::CaGenerate.generate_ca_pair(Fluent::CaGenerate::DEFAULT_OPTIONS)
     assert_equal(OpenSSL::X509::Certificate, cert.class)
     assert_true(key.private?)
   end
