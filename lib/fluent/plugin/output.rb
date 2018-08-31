abbcf5c468acc8a7248edef1e33c2b8958382c53
Masahiro Nakagawa <repeatedly@gmail.com>
2018-08-31 17:04:20 +0900

    output: Add disable_chunk_backup for ignore broken chunks. fix #2112
    Signed-off-by: Masahiro Nakagawa <repeatedly@gmail.com>

--- lib/fluent/plugin/output.rb
+++ lib/fluent/plugin/output.rb
@@ -96,6 +96,7 @@ module Fluent
         config_param :retry_max_interval, :time, default: nil, desc: 'The maximum interval seconds for exponential backoff between retries while failing.'
 
         config_param :retry_randomize, :bool, default: true, desc: 'If true, output plugin will retry after randomized interval not to do burst retries.'
+        config_param :disable_chunk_backup, :bool, default: false, desc: 'If true, chunks are thrown away when unrecoverable error happens'
       end
 
       config_section :secondary, param_name: :secondary_config, required: false, multi: false, final: true do
@@ -1161,17 +1162,21 @@ module Fluent
       end
 
       def backup_chunk(chunk, using_secondary, delayed_commit)
-        unique_id = dump_unique_id_hex(chunk.unique_id)
-        safe_plugin_id = plugin_id.gsub(/[ "\/\\:;|*<>?]/, '_')
-        backup_base_dir = system_config.root_dir || DEFAULT_BACKUP_DIR
-        backup_file = File.join(backup_base_dir, 'backup', "worker#{fluentd_worker_id}", safe_plugin_id, "#{unique_id}.log")
-        backup_dir = File.dirname(backup_file)
+        if @buffer_config.disable_chunk_backup
+          log.warn "disable_chunk_backup is true. #{dump_unique_id_hex(chunk.unique_id)} chunk is thrown away"
+        else
+          unique_id = dump_unique_id_hex(chunk.unique_id)
+          safe_plugin_id = plugin_id.gsub(/[ "\/\\:;|*<>?]/, '_')
+          backup_base_dir = system_config.root_dir || DEFAULT_BACKUP_DIR
+          backup_file = File.join(backup_base_dir, 'backup', "worker#{fluentd_worker_id}", safe_plugin_id, "#{unique_id}.log")
+          backup_dir = File.dirname(backup_file)
 
-        log.warn "bad chunk is moved to #{backup_file}"
-        FileUtils.mkdir_p(backup_dir) unless Dir.exist?(backup_dir)
-        File.open(backup_file, 'ab', system_config.file_permission || 0644) { |f|
-          chunk.write_to(f)
-        }
+          log.warn "bad chunk is moved to #{backup_file}"
+          FileUtils.mkdir_p(backup_dir) unless Dir.exist?(backup_dir)
+          File.open(backup_file, 'ab', system_config.file_permission || 0644) { |f|
+            chunk.write_to(f)
+          }
+        end
         commit_write(chunk.unique_id, secondary: using_secondary, delayed: delayed_commit)
       end
 
