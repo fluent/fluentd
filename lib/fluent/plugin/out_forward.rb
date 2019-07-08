ce775b7c49a576669828083df4d38afe3d2cfac8
Yuta Iwama <ganmacs@gmail.com>
2019-07-08 17:05:04 +0900

    Separate SocketCache into file
    Signed-off-by: Yuta Iwama <ganmacs@gmail.com>

--- lib/fluent/plugin/out_forward.rb
+++ lib/fluent/plugin/out_forward.rb
@@ -21,6 +21,7 @@ require 'base64'
 
 require 'fluent/compat/socket_util'
 require 'fluent/plugin/out_forward/load_balancer'
+require 'fluent/plugin/out_forward/socket_cache'
 
 module Fluent::Plugin
   class ForwardOutput < Output
@@ -510,139 +511,6 @@ module Fluent::Plugin
     end
 
     class Node
-      class SocketCache
-        TimedSocket = Struct.new(:timeout, :sock, :ref)
-
-        def initialize(timeout, log)
-          @log = log
-          @timeout = timeout
-          @active_socks = {}
-          @inactive_socks = {}
-          @mutex = Mutex.new
-        end
-
-        def revoke(key = Thread.current.object_id)
-          @mutex.synchronize do
-            if @active_socks[key]
-              @inactive_socks[key] = @active_socks.delete(key)
-              @inactive_socks[key].ref = 0
-            end
-          end
-        end
-
-        def clear
-          @mutex.synchronize do
-            @inactive_socks.values.each do |s|
-              s.sock.close rescue nil
-            end
-            @inactive_socks.clear
-
-            @active_socks.values.each do |s|
-              s.sock.close rescue nil
-            end
-            @active_socks.clear
-          end
-        end
-
-        def purge_obsolete_socks
-          @mutex.synchronize do
-            @inactive_socks.keys.each do |k|
-              # 0 means sockets stored in this class received all acks
-              if @inactive_socks[k].ref <= 0
-                s = @inactive_socks.delete(k)
-                s.sock.close  rescue nil
-                @log.debug("purged obsolete socket #{s.sock}")
-              end
-            end
-
-            @active_socks.keys.each do |k|
-              if expired?(k) && @active_socks[k].ref <= 0
-                @inactive_socks[k] = @active_socks.delete(k)
-              end
-            end
-          end
-        end
-
-        # We expect that `yield` returns a unique object in this class
-        def fetch_or(key = Thread.current.object_id)
-          @mutex.synchronize do
-            unless @active_socks[key]
-              @active_socks[key] = TimedSocket.new(timeout, yield, 1)
-              @log.debug("connect new socket #{@active_socks[key]}")
-              return @active_socks[key].sock
-            end
-
-            if expired?(key)
-              # Do not close this socket here in case of it will be used by other place (e.g. wait for receiving ack)
-              @inactive_socks[key] = @active_socks.delete(key)
-              @log.debug("connection #{@inactive_socks[key]} is expired. reconnecting...")
-              @active_socks[key] = TimedSocket.new(timeout, yield, 0)
-            end
-
-            @active_socks[key].ref += 1
-            @active_socks[key].sock
-          end
-        end
-
-        def dec_ref(key = Thread.current.object_id)
-          @mutex.synchronize do
-            if @active_socks[key]
-              @active_socks[key].ref -= 1
-            elsif @inactive_socks[key]
-              @inactive_socks[key].ref -= 1
-            else
-              @log.warn("Not found key for dec_ref: #{key}")
-            end
-          end
-        end
-
-        # This method is expected to be called in class which doesn't call #inc_ref
-        def dec_ref_by_value(val)
-          @mutex.synchronize do
-            sock = @active_socks.detect { |_, v| v.sock == val }
-            if sock
-              key = sock.first
-              @active_socks[key].ref -= 1
-              return
-            end
-
-            sock = @inactive_socks.detect { |_, v| v.sock == val }
-            if sock
-              key = sock.first
-              @inactive_socks[key].ref -= 1
-              return
-            else
-              @log.warn("Not found key for dec_ref_by_value: #{key}")
-            end
-          end
-        end
-
-        # This method is expected to be called in class which doesn't call #fetch_or
-        def revoke_by_value(val)
-          @mutex.synchronize do
-            sock = @active_socks.detect { |_, v| v.sock == val }
-            if sock
-              key = sock.first
-              @inactive_socks[key] = @active_socks.delete(key)
-              @inactive_socks[key].ref = 0
-            else
-              @log.debug("Not found for revoke_by_value :#{val}")
-            end
-          end
-        end
-
-        private
-
-        def timeout
-          @timeout && Time.now + @timeout
-        end
-
-        # This method is thread unsafe
-        def expired?(key = Thread.current.object_id)
-           @active_socks[key].timeout ? @active_socks[key].timeout < Time.now : false
-        end
-      end
-
       # @param keepalive [Bool]
       # @param keepalive_timeout [Integer | nil]
       def initialize(sender, server, failure:, keepalive: false, keepalive_timeout: nil)
@@ -681,7 +549,7 @@ module Fluent::Plugin
 
         @keepalive = keepalive
         if @keepalive
-          @socket_cache = SocketCache.new(keepalive_timeout, @log)
+          @socket_cache = ForwardOutput::SocketCache.new(keepalive_timeout, @log)
         end
       end
 
---
+++ test/plugin/out_forward/test_socket_cache.rb
@@ -0,0 +1,132 @@
+require_relative '../../helper'
+
+require 'fluent/plugin/out_forward/socket_cache'
+
+class SocketCacheTest < Test::Unit::TestCase
+  sub_test_case 'fetch_or' do
+    test 'when gived key does not exist' do
+      c = Fluent::Plugin::ForwardOutput::SocketCache.new(10, Logger.new(nil))
+      sock = mock!.open { 1 }.subject
+      assert_equal(1, c.fetch_or { sock.open })
+    end
+
+    test 'when given key exists' do
+      c = Fluent::Plugin::ForwardOutput::SocketCache.new(10, Logger.new(nil))
+      assert_equal(1, c.fetch_or { 1 })
+
+      sock = dont_allow(mock!).open
+      assert_equal(1, c.fetch_or { sock.open })
+    end
+
+    test "when given key's value was expired" do
+      c = Fluent::Plugin::ForwardOutput::SocketCache.new(0, Logger.new(nil))
+      assert_equal(1, c.fetch_or { 1 })
+
+      sock = mock!.open { 1 }.subject
+      assert_equal(1, c.fetch_or { sock.open })
+    end
+  end
+
+  test 'revoke' do
+    c = Fluent::Plugin::ForwardOutput::SocketCache.new(10, Logger.new(nil))
+    c.fetch_or { 1 }
+    c.revoke
+
+    sock = mock!.open { 1 }.subject
+    assert_equal(1, c.fetch_or { sock.open })
+  end
+
+  test 'revoke_by_value' do
+    c = Fluent::Plugin::ForwardOutput::SocketCache.new(10, Logger.new(nil))
+    c.fetch_or { 1 }
+    c.revoke_by_value(1)
+
+    sock = mock!.open { 1 }.subject
+    assert_equal(1, c.fetch_or { sock.open })
+  end
+
+  sub_test_case 'dec_ref' do
+    test 'when value exists in active_socks' do
+      c = Fluent::Plugin::ForwardOutput::SocketCache.new(10, Logger.new(nil))
+      c.fetch_or { 1 }
+      c.dec_ref
+
+      assert_equal(0, c.instance_variable_get(:@active_socks)[Thread.current.object_id].ref)
+    end
+
+    test 'when value exists in inactive_socks' do
+      c = Fluent::Plugin::ForwardOutput::SocketCache.new(10, Logger.new(nil))
+      c.fetch_or { 1 }
+      c.revoke
+      c.dec_ref
+      assert_equal(-1, c.instance_variable_get(:@inactive_socks)[Thread.current.object_id].ref)
+    end
+  end
+
+  sub_test_case 'dec_ref_by_value' do
+    test 'when value exists in active_socks' do
+      c = Fluent::Plugin::ForwardOutput::SocketCache.new(10, Logger.new(nil))
+      c.fetch_or { 1 }
+      c.dec_ref_by_value(1)
+
+      assert_equal(0, c.instance_variable_get(:@active_socks)[Thread.current.object_id].ref)
+    end
+
+    test 'when value exists in inactive_socks' do
+      c = Fluent::Plugin::ForwardOutput::SocketCache.new(10, Logger.new(nil))
+      c.fetch_or { 1 }
+      c.revoke
+      c.dec_ref_by_value(1)
+      assert_equal(-1, c.instance_variable_get(:@inactive_socks)[Thread.current.object_id].ref)
+    end
+  end
+
+  sub_test_case 'clear' do
+    test 'when value is in active_socks' do
+      c = Fluent::Plugin::ForwardOutput::SocketCache.new(10, Logger.new(nil))
+      m = mock!.close { 'closed' }.subject
+      c.fetch_or { m }
+      assert_true(!c.instance_variable_get(:@active_socks).empty?)
+
+      c.clear
+      assert_true(c.instance_variable_get(:@active_socks).empty?)
+    end
+
+    test 'when value is in inactive_socks' do
+      c = Fluent::Plugin::ForwardOutput::SocketCache.new(10, Logger.new(nil))
+      m = mock!.close { 'closed' }.subject
+      c.fetch_or { m }
+      c.revoke
+      assert_true(!c.instance_variable_get(:@inactive_socks).empty?)
+
+      c.clear
+      assert_true(c.instance_variable_get(:@active_socks).empty?)
+    end
+  end
+
+  sub_test_case 'purge_obsolete_socks' do
+    test 'delete key in inactive_socks' do
+      c = Fluent::Plugin::ForwardOutput::SocketCache.new(10, Logger.new(nil))
+      m = mock!.close { 'closed' }.subject
+      c.fetch_or { m }
+      c.revoke
+      assert_true(!c.instance_variable_get(:@inactive_socks).empty?)
+
+      c.purge_obsolete_socks
+      assert_true(c.instance_variable_get(:@active_socks).empty?)
+    end
+
+    test 'move key from active_socks to inactive_socks' do
+      c = Fluent::Plugin::ForwardOutput::SocketCache.new(10, Logger.new(nil))
+      m = dont_allow(mock!).close
+      stub(m).inspect         # for log
+      c.fetch_or { m }
+      assert_true(!c.instance_variable_get(:@active_socks).empty?)
+      assert_true(c.instance_variable_get(:@inactive_socks).empty?)
+
+      c.purge_obsolete_socks
+      assert_true(!c.instance_variable_get(:@active_socks).empty?)
+      assert_true(c.instance_variable_get(:@inactive_socks).empty?)
+    end
+  end
+end
