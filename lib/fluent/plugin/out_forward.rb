09abcbaea0f703d761cbc19a7ace10833e3590cd
Yuta Iwama <ganmacs@gmail.com>
2019-07-09 17:46:09 +0900

    Pass variable to use in check_pong
    Signed-off-by: Yuta Iwama <ganmacs@gmail.com>

--- lib/fluent/plugin/out_forward.rb
+++ lib/fluent/plugin/out_forward.rb
@@ -537,7 +537,7 @@ module Fluent::Plugin
         @username = server.username
         @password = server.password
         @shared_key = server.shared_key || (sender.security && sender.security.shared_key) || ""
-        @handshake = HandshakeProtocol.new
+        @handshake = HandshakeProtocol.new(log: @log, security: @sender.security, shared_key: @shared_key)
 
         @unpacker = Fluent::Engine.msgpack_unpacker
 
@@ -588,12 +588,15 @@ module Fluent::Plugin
       class HandshakeProtocol
         attr_reader :shared_key_salt
 
-        def initialize
+        def initialize(log:, security:, shared_key:)
+          @log = log
+          @security = security
+          @shared_key = shared_key
           @shared_key_salt = generate_salt
         end
 
         def check_pong(ri, message)
-          @log.debug "checking pong"
+          @log.debug('checking pong')
           # ['PONG', bool(authentication result), 'reason if authentication failed',
           #  self_hostname, sha512\_hex(salt + self_hostname + nonce + sharedkey)]
           unless message.size == 5 && message[0] == 'PONG'
@@ -605,16 +608,16 @@ module Fluent::Plugin
             return false, 'authentication failed: ' + reason
           end
 
-          if hostname == @sender.security.self_hostname
+          if hostname == @security.self_hostname
             return false, 'same hostname between input and output: invalid configuration'
           end
 
-          clientside = Digest::SHA512.new.update(@handshake.shared_key_salt).update(hostname).update(ri.shared_key_nonce).update(@shared_key).hexdigest
+          clientside = Digest::SHA512.new.update(@shared_key_salt).update(hostname).update(ri.shared_key_nonce).update(@shared_key).hexdigest
           unless shared_key_hexdigest == clientside
             return false, 'shared key mismatch'
           end
 
-          return true, nil
+          [true, nil]
         end
 
         private
