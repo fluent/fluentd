f32e9539459a9d86e40344c9086537e0cd9b16ed
Yuta Iwama <ganmacs@gmail.com>
2019-07-12 13:50:56 +0900

    ruby 2.1 can't allow to use these syntax
    > /home/travis/build/fluent/fluentd/test/plugin/out_forward/test_load_balancer.rb:10: Can't assign to false
    ...mock('node', 'standby?': false, 'available?': false, weight:...
    
    https://travis-ci.org/fluent/fluentd/jobs/557649271
    
    Signed-off-by: Yuta Iwama <ganmacs@gmail.com>

--- test/plugin/out_forward/test_load_balancer.rb
+++ test/plugin/out_forward/test_load_balancer.rb
@@ -7,8 +7,8 @@ class LoadBalancerTest < Test::Unit::TestCase
   sub_test_case 'select_healthy_node' do
     test 'select healty node' do
       lb = Fluent::Plugin::ForwardOutput::LoadBalancer.new($log)
-      n1 = flexmock('node', 'standby?': false, 'available?': false, weight: 1)
-      n2 = flexmock('node', 'standby?': false, 'available?': true, weight: 1)
+      n1 = flexmock('node', :'standby?' => false, :'available?' => false, weight: 1)
+      n2 = flexmock('node', :'standby?' => false, :'available?' => true, weight: 1)
 
       lb.rebuild_weight_array([n1, n2])
       lb.select_healthy_node do |node|
@@ -22,8 +22,8 @@ class LoadBalancerTest < Test::Unit::TestCase
 
     test 'call like round robin' do
       lb = Fluent::Plugin::ForwardOutput::LoadBalancer.new($log)
-      n1 = flexmock('node', 'standby?': false, 'available?': true, weight: 1)
-      n2 = flexmock('node', 'standby?': false, 'available?': true, weight: 1)
+      n1 = flexmock('node', :'standby?' => false, :'available?' => true, weight: 1)
+      n2 = flexmock('node', :'standby?' => false, :'available?' => true, weight: 1)
 
       lb.rebuild_weight_array([n1, n2])
 
@@ -51,7 +51,7 @@ class LoadBalancerTest < Test::Unit::TestCase
 
     test 'raise an error if all node are unavialble' do
       lb = Fluent::Plugin::ForwardOutput::LoadBalancer.new($log)
-      lb.rebuild_weight_array([flexmock('node', 'standby?': false, 'available?': false, weight: 1)])
+      lb.rebuild_weight_array([flexmock('node', :'standby?' => false, :'available?' => false, weight: 1)])
       assert_raise(Fluent::Plugin::ForwardOutput::NoNodesAvailable) do
         lb.select_healthy_node
       end
