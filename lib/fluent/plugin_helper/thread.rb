0cc564e4a6a4e661f5e2840c6e69ea24c5ff157f
Javier Kohen <jkohen@google.com>
2019-08-16 12:43:41 -0400

    Set thread title
    This helps understand thread usage with commands like `top -H` and `ps -e -T`
    
    Signed-off-by: Javier Kohen <jkohen@google.com>

--- lib/fluent/plugin_helper/thread.rb
+++ lib/fluent/plugin_helper/thread.rb
@@ -92,6 +92,7 @@ module Fluent
           end
         end
         thread.abort_on_exception = true
+        thread.name = title
         @_threads_mutex.synchronize do
           @_threads[thread.object_id] = thread
         end
