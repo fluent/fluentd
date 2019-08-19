f41435b0e0b13c724eceb8aaef88491bb2f58799
Javier Kohen <jkohen@google.com>
2019-08-19 09:24:36 -0400

    Only set thread name when the method is available.
    Signed-off-by: Javier Kohen <jkohen@google.com>

--- lib/fluent/plugin_helper/thread.rb
+++ lib/fluent/plugin_helper/thread.rb
@@ -92,7 +92,7 @@ module Fluent
           end
         end
         thread.abort_on_exception = true
-        thread.name = title.to_s
+        thread.name = title.to_s if thread.respond_to?(:name)
         @_threads_mutex.synchronize do
           @_threads[thread.object_id] = thread
         end
