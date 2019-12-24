d5038a1674da3825ce253c603f3abbc3ae487721
Masahiro Nakagawa <repeatedly@gmail.com>
2019-12-24 18:54:56 +0900

    out_copy: Add copy_mode parameter. fix #2744
    Signed-off-by: Masahiro Nakagawa <repeatedly@gmail.com>

--- lib/fluent/plugin/out_copy.rb
+++ lib/fluent/plugin/out_copy.rb
@@ -23,7 +23,9 @@ module Fluent::Plugin
     Fluent::Plugin.register_output('copy', self)
 
     desc 'If true, pass different record to each `store` plugin.'
-    config_param :deep_copy, :bool, default: false
+    config_param :deep_copy, :bool, default: false, deprecated: "use 'copy_mode' parameter instead"
+    desc 'Pass different record to each `store` plugin by specified method'
+    config_param :copy_mode, :enum, list: [:no_copy, :shallow, :deep, :marshal], default: :no_copy
 
     attr_reader :ignore_errors
 
@@ -35,6 +37,7 @@ module Fluent::Plugin
     def configure(conf)
       super
 
+      @copy_proc = gen_copy_proc
       @stores.each { |store|
         @ignore_errors << (store.arg == 'ignore_error')
       }
@@ -55,7 +58,7 @@ module Fluent::Plugin
 
       outputs.each_with_index do |output, i|
         begin
-          output.emit_events(tag, @deep_copy ? es.dup : es)
+          output.emit_events(tag, @copy_proc ? @copy_proc.call(es) : es)
         rescue => e
           if @ignore_errors[i]
             log.error "ignore emit error", error: e
@@ -63,6 +66,41 @@ module Fluent::Plugin
             raise e
           end
         end
+      end
+    end
+
+    private
+
+    def gen_copy_proc
+      @copy_mode = :shallow if @deep_copy
+
+      case @copy_mode
+      when :no_copy
+         nil
+      when :shallow
+        Proc.new { |es| es.dup }
+      when :deep
+        Proc.new { |es|
+          packer = Fluent::MessagePackFactory.msgpack_packer
+          times = []
+          records = []
+          es.each { |time, record|
+            times << time
+            packer.pack(record)
+          }
+          Fluent::MessagePackFactory.msgpack_unpacker.feed_each(packer.full_pack) { |record|
+            records << record
+          }
+          Fluent::MultiEventStream.new(times, records)
+        }
+      when :marshal
+        Proc.new { |es|
+          new_es = Fluent::MultiEventStream.new
+          es.each { |time, record|
+            new_es.add(time, Marshal.load(Marshal.dump(record)))
+          }
+          new_es
+        }
       end
     end
   end
