790b896192004dc70b3c98be1a428636fb8e7aa4
Hiroshi Hatake <hatake@calyptia.com>
2021-06-04 14:23:18 +0900

    system_config: Add without_input_metrics system config parameter
    Input metrics should be paying high CPU cost due to run data size
    counter for ingestion logs.
    This parameter can handle to turn on/off for measuring input plugin metrics.
    
    Signed-off-by: Hiroshi Hatake <hatake@calyptia.com>

--- lib/fluent/system_config.rb
+++ lib/fluent/system_config.rb
@@ -27,7 +27,8 @@ module Fluent
       :log_event_verbose, :ignore_repeated_log_interval, :ignore_same_log_interval,
       :without_source, :rpc_endpoint, :enable_get_dump, :process_name,
       :file_permission, :dir_permission, :counter_server, :counter_client,
-      :strict_config_value, :enable_msgpack_time_support, :disable_shared_socket
+      :strict_config_value, :enable_msgpack_time_support, :disable_shared_socket,
+      :without_input_metrics
     ]
 
     config_param :workers,   :integer, default: 1
@@ -46,6 +47,7 @@ module Fluent
     config_param :strict_config_value, :bool, default: nil
     config_param :enable_msgpack_time_support, :bool, default: nil
     config_param :disable_shared_socket, :bool, default: nil
+    config_param :without_input_metrics, :bool, default: true
     config_param :file_permission, default: nil do |v|
       v.to_i(8)
     end
