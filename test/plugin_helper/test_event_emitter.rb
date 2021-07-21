85b11046157680b6134c31fee52f83df64cee2a0
Takuro Ashie <ashie@clear-code.com>
2021-07-21 14:11:23 +0900

    PluginHelper::EventEmitter: Add tests for event_emitter_router
    Signed-off-by: Takuro Ashie <ashie@clear-code.com>

--- test/plugin_helper/test_event_emitter.rb
+++ test/plugin_helper/test_event_emitter.rb
@@ -1,6 +1,7 @@
 require_relative '../helper'
 require 'fluent/plugin_helper/event_emitter'
 require 'fluent/plugin/base'
+require 'flexmock/test_unit'
 
 class EventEmitterTest < Test::Unit::TestCase
   setup do
@@ -47,5 +48,33 @@ class EventEmitterTest < Test::Unit::TestCase
     assert_nil d1.router
 
     d1.terminate
+  end
+
+  test 'should not have event_emitter_router' do
+    d0 = Dummy0.new
+    assert !d0.respond_to?(:event_emitter_router)
+  end
+
+  test 'should have event_emitter_router' do
+    d0 = Dummy.new
+    assert d0.respond_to?(:event_emitter_router)
+  end
+
+  test 'get router' do
+    router_mock = flexmock('mytest')
+    label_mock = flexmock('mylabel')
+    label_mock.should_receive(:event_router).twice.and_return(router_mock)
+    Fluent::Engine.root_agent.labels['@mytest'] = label_mock
+
+    d = Dummy.new
+    d.configure(config_element('ROOT', '', {'@label' => '@mytest'}))
+    router = d.event_emitter_router("@mytest")
+    assert_equal router_mock, router
+  end
+
+  test 'get root router' do
+    d = Dummy.new
+    router = d.event_emitter_router("@ROOT")
+    assert_equal Fluent::Engine.root_agent.event_router, router
   end
 end
