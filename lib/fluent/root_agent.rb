62809ca855da9c2491570d9aed499c975c5ad9bf
Takuro Ashie <ashie@clear-code.com>
2021-07-30 15:07:36 +0900

    Add more tests for invalid <label> directive
    Signed-off-by: Takuro Ashie <ashie@clear-code.com>

--- lib/fluent/root_agent.rb
+++ lib/fluent/root_agent.rb
@@ -131,7 +131,7 @@ module Fluent
         end
         name = e.arg
         raise ConfigError, "Missing symbol argument on <label> directive" if name.empty?
-        raise ConfigError, "@ROOT for <label> is invalid, reserved for getting root router" if name == '@ROOT'
+        raise ConfigError, "@ROOT for <label> is not permitted, reserved for getting root router" if name == '@ROOT'
 
         if name == ERROR_LABEL
           error_label_config = e
