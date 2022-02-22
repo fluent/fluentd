01dceab106033c7a1c0b1587865340bd21601d6e
Takuro Ashie <ashie@clear-code.com>
2022-02-23 01:41:35 +0900

    Support IPv6 address for rpc_endpoint in system config
    Signed-off-by: Takuro Ashie <ashie@clear-code.com>

--- lib/fluent/rpc.rb
+++ lib/fluent/rpc.rb
@@ -20,9 +20,10 @@ module Fluent
   module RPC
     class Server
       def initialize(endpoint, log)
-        bind, port = endpoint.split(':')
-        @bind = bind
-        @port = port
+        m = endpoint.match(/^\[?(?<host>[0-9a-zA-Z:\-\.]+)\]?:(?<port>[0-9]+)$/)
+        raise Fluent::ConfigError, "Invalid rpc_endpoint: #{endpoint}" unless m
+        @bind = m[:host]
+        @port = m[:port]
         @log = log
 
         @server = WEBrick::HTTPServer.new(
