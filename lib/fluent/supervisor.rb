337b0998932f1a6a69c70fd1afadceb4a8af98f1
Daijiro Fukuda <fukuda@clear-code.com>
2022-03-22 00:32:15 +0900

    Add rpc `api/processes.dump`
    Signed-off-by: Daijiro Fukuda <fukuda@clear-code.com>

--- lib/fluent/supervisor.rb
+++ lib/fluent/supervisor.rb
@@ -106,6 +106,11 @@ module Fluent
         end
         nil
       }
+      @rpc_server.mount_proc('/api/processes.dump') { |req, res|
+        $log.debug "fluentd RPC got /api/processes.dump request"
+        supervisor_sigcont_handler
+        nil
+      }
       @rpc_server.mount_proc('/api/plugins.flushBuffers') { |req, res|
         $log.debug "fluentd RPC got /api/plugins.flushBuffers request"
         if Fluent.windows?
