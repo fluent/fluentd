a9bfed884067c4c114f9d449f74ab3f8eb1ec6e5
Fujimoto Seiji <fujimoto@ceptord.net>
2022-08-13 17:45:38 +0900

    Add a new system config 'enable_jit'
    This adds a new option to enable Ruby JIT in worker processes.
    
    The exact JIT strategy (in particular mjit or yjit) will be chosen
    by Ruby as it sees fit. See the following ticket for details:
    
        https://bugs.ruby-lang.org/issues/18349
    
    Signed-off-by: Fujimoto Seiji <fujimoto@ceptord.net>

--- test/config/test_system_config.rb
+++ test/config/test_system_config.rb
@@ -84,6 +84,7 @@ module Fluent::Config
       assert_nil(sc.enable_input_metrics)
       assert_nil(sc.enable_size_metrics)
       assert_nil(sc.enable_msgpack_time_support)
+      assert(!sc.enable_jit)
       assert_equal(:text, sc.log.format)
       assert_equal('%Y-%m-%d %H:%M:%S %z', sc.log.time_format)
     end
@@ -102,6 +103,7 @@ module Fluent::Config
       'enable_msgpack_time_support' => ['enable_msgpack_time_support', true],
       'enable_input_metrics' => ['enable_input_metrics', true],
       'enable_size_metrics' => ['enable_size_metrics', true],
+      'enable_jit' => ['enable_jit', true],
     )
     test "accepts parameters" do |(k, v)|
       conf = parse_text(<<-EOS)
