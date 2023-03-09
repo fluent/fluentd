041af39f401b26002c07a299ad7c85346909ce7c
Daijiro Fukuda <fukuda@clear-code.com>
2023-03-09 14:15:00 +0900

    Refactor Superviser.load_config()
    It does not load the config anymore since #2709, so it should
    simply returns the config for ServerEngine.
    
    Signed-off-by: Daijiro Fukuda <fukuda@clear-code.com>

--- lib/fluent/daemon.rb
+++ lib/fluent/daemon.rb
@@ -9,7 +9,5 @@ require 'fluent/supervisor'
 
 server_module = Fluent.const_get(ARGV[0])
 worker_module = Fluent.const_get(ARGV[1])
-# it doesn't call ARGV in block because when reloading config, params will be initialized and then it can't use previous config.
-config_path = ARGV[2]
-params = JSON.parse(ARGV[3])
-ServerEngine::Daemon.run_server(server_module, worker_module) { Fluent::Supervisor.load_config(config_path, params) }
+params = JSON.parse(ARGV[2])
+ServerEngine::Daemon.run_server(server_module, worker_module) { Fluent::Supervisor.serverengine_config(params) }
