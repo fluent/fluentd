79220ab40c45f9316ccb7879e1490accf3052f84
Šimon Lukašík <isimluk@fedoraproject.org>
2023-09-08 23:39:38 +0200

    perf: use unpack1 instead of unpack..first
    Signed-off-by: Šimon Lukašík <isimluk@fedoraproject.org>

--- lib/fluent/plugin/buffer/file_chunk.rb
+++ lib/fluent/plugin/buffer/file_chunk.rb
@@ -399,7 +399,7 @@ module Fluent
           end
 
           if chunk.slice(0, 2) == BUFFER_HEADER
-            size = chunk.slice(2, 4).unpack('N').first
+            size = chunk.slice(2, 4).unpack1('N')
             if size
               return Fluent::MessagePackFactory.msgpack_unpacker(symbolize_keys: true).feed(chunk.slice(6, size)).read rescue nil
             end
--- lib/fluent/unique_id.rb
+++ lib/fluent/unique_id.rb
@@ -23,7 +23,7 @@ module Fluent
     end
 
     def self.hex(unique_id)
-      unique_id.unpack('H*').first
+      unique_id.unpack1('H*')
     end
 
     module Mixin
