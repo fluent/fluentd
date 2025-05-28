0c05571fb839ffa23922661cf4d8977ebbb0d00e
Shizuo Fujita <fujita@clear-code.com>
2025-05-28 14:39:45 +0900

    test_output_as_buffered_secondary: use getter method
    Signed-off-by: Shizuo Fujita <fujita@clear-code.com>

--- test/plugin/test_output_as_buffered_secondary.rb
+++ test/plugin/test_output_as_buffered_secondary.rb
@@ -308,7 +308,7 @@ class BufferedOutputSecondaryTest < Test::Unit::TestCase
 
       assert_equal 0, @i.write_count
       assert_equal 0, @i.num_errors
-      assert_equal 0, @i.instance_variable_get(:@write_secondary_count_metrics).get
+      assert_equal 0, @i.write_secondary_count
 
       @i.enqueue_thread_wait
       @i.flush_thread_wakeup
@@ -349,7 +349,7 @@ class BufferedOutputSecondaryTest < Test::Unit::TestCase
       assert{ @i.write_count > prev_write_count }
       assert{ @i.num_errors == prev_num_errors }
 
-      assert{ @i.instance_variable_get(:@write_secondary_count_metrics).get > 0 }
+      assert{ @i.write_secondary_count > 0 }
 
       assert_nil @i.retry
 
@@ -446,7 +446,7 @@ class BufferedOutputSecondaryTest < Test::Unit::TestCase
       assert{ @i.buffer.dequeued[chunks[0].unique_id].nil? }
       assert{ chunks.first.empty? }
 
-      assert{ @i.instance_variable_get(:@write_secondary_count_metrics).get > 0 }
+      assert{ @i.write_secondary_count > 0 }
 
       assert_nil @i.retry
 
@@ -742,7 +742,7 @@ class BufferedOutputSecondaryTest < Test::Unit::TestCase
 
       assert_equal 0, @i.write_count
       assert_equal 0, @i.num_errors
-      assert_equal 0, @i.instance_variable_get(:@write_secondary_count_metrics).get
+      assert_equal 0, @i.write_secondary_count
 
       @i.enqueue_thread_wait
       @i.flush_thread_wakeup
@@ -771,7 +771,7 @@ class BufferedOutputSecondaryTest < Test::Unit::TestCase
         prev_write_count = @i.write_count
       end
 
-      assert{ @i.instance_variable_get(:@write_secondary_count_metrics).get > 0 }
+      assert{ @i.write_secondary_count > 0 }
 
       # retry_timeout == 60(sec), retry_secondary_threshold == 0.8
 
