265defe83ee4d3c109d5c54c2d42737248ff9603
Shizuo Fujita <fujita@clear-code.com>
2025-06-05 11:21:14 +0900

    Generate warning messages according to log_level with 'pattern not matched'
    Signed-off-by: Shizuo Fujita <fujita@clear-code.com>

--- lib/fluent/plugin/in_udp.rb
+++ lib/fluent/plugin/in_udp.rb
@@ -83,7 +83,7 @@ module Fluent::Plugin
         begin
           @parser.parse(data) do |time, record|
             unless time && record
-              log.warn "pattern not matched", data: data
+              log.on_warn { log.warn "pattern not matched", data: data }
               next
             end
 
