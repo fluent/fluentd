496b8ac30ce4d249e496c56c45b13735b85063da
Shizuo Fujita <fujita@clear-code.com>
2026-09-02 10:11:29 +0900

    out_http: remove the Net::HTTP extra_chain_cert monkey patch
    Net::HTTP supports extra_chain_cert since Ruby 3.0 (Feature #9758),
    and fluentd now requires Ruby 3.3 or later, so the patch added in
    PR #3146 is dead code.
    
    Signed-off-by: Shizuo Fujita <fujita@clear-code.com>

--- lib/fluent/plugin/out_http.rb
+++ lib/fluent/plugin/out_http.rb
@@ -22,16 +22,6 @@ require 'fluent/tls'
 require 'fluent/plugin/output'
 require 'fluent/plugin_helper/socket'
 
-# patch Net::HTTP to support extra_chain_cert which was added in Ruby feature #9758.
-# see: https://github.com/ruby/ruby/commit/31af0dafba6d3769d2a39617c0dddedb97883712
-unless Net::HTTP::SSL_IVNAMES.include?(:@extra_chain_cert)
-  class Net::HTTP
-    SSL_IVNAMES << :@extra_chain_cert
-    SSL_ATTRIBUTES << :extra_chain_cert
-    attr_accessor :extra_chain_cert
-  end
-end
-
 module Fluent::Plugin
   class HTTPOutput < Output
     Fluent::Plugin.register_output('http', self)
